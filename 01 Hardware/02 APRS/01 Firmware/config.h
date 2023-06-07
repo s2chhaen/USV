e6a7b3cde02d77dbab7c1de00fdf4d1999e2505f
Joe G <grabow@amesys.de>
2023-06-07 16:40:45 +0200

    Signed-off-by: Joe G <grabow@amesys.de>

---
+++ 01 Hardware/02 APRS/01 Firmware/config.h
@@ -0,0 +1,10 @@
+// Copyright Mark Qvist / unsigned.io
+// https://unsigned.io/microaprs
+//
+// Licensed under GPL-3.0. For full info,
+// read the LICENSE file.
+
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#endif
---
+++ 01 Hardware/02 APRS/01 Firmware/util/constants.h
@@ -0,0 +1,15 @@
+// Copyright Mark Qvist / unsigned.io
+// https://unsigned.io/microaprs
+//
+// Licensed under GPL-3.0. For full info,
+// read the LICENSE file.
+
+#define PROTOCOL_KISS 0x01
+#define PROTOCOL_SIMPLE_SERIAL 0x02
+
+#define m328p  0x01
+#define m1284p 0x02
+#define m644p  0x03
+
+#define REF_3V3 0x01
+#define REF_5V  0x02
