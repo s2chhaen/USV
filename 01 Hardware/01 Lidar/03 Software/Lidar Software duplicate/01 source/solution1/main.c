c609bf92c2afca059a6ce5bd4faa6ab2d706b62d
hathach23 <53267440+hathach23@users.noreply.github.com>
2023-06-16 17:47:14 +0200

    Hinzufuege checksumCRC16() Funktion
    Durch Anpassung des Muster-Muster-Program in Buch von Hersteller und Anpassung der Global-Variablen-Type

--- 01 Hardware/01 Lidar/03 Software/Lidar Software duplicate/01 source/solution1/main.c
+++ 01 Hardware/01 Lidar/03 Software/Lidar Software duplicate/01 source/solution1/main.c
@@ -37,6 +37,26 @@ volatile uint8_t *converted_distances;
 volatile bool state = false;
 
 
+
+//Implementation von CRC16-Checksum durch Anpassung Muster-Program in Buch "Telegramme zur Konfiguration und//Bedienung der Lasermesssysteme LMS2xx" von Hersteller
+uint16_t checksumCrc16(uint8_t input[], uint32_t length){
+	const uint16_t genPolynom = 0x8005;
+	uint16_t uCrc16=0;
+	uint8_t temp[]={0,0};
+	for (uint32_t i=0;i<length;i++){
+		temp[1]=temp[0];
+		temp[0]=input[i];
+		if (uCrc16&0x8000){
+			uCrc16 = (uCrc16&0x7fff)<<1;
+			uCrc16 ^= genPolynom;
+			} else{
+			uCrc16<<=1;
+		}
+		uCrc16 ^= (input[0]|(input[1]<<8));
+	}
+	return uCrc16;
+}
+
 /*daten_empfangen**********************************************************
 *Input:
 *uint8_t adress
@@ -78,9 +98,9 @@ bool daten_empfangen(uint8_t adress, uint8_t data[], uint8_t length)
 	for (int n=i; n<i+722; n++) {
 		filtered_data[n] = datastream[n];
 	}
-	distances = bytes_to_values(filtered_data);
-	rounded_distances = round_values(distances);
-	converted_distances = cm_to_m(rounded_distances);
+	distances = bytes_to_values((uint8_t *)filtered_data);
+	rounded_distances = round_values((uint8_t *)distances);
+	converted_distances = cm_to_m((uint8_t *)rounded_distances);
 	state = true;
 	
 	return true;
@@ -90,8 +110,8 @@ bool daten_empfangen(uint8_t adress, uint8_t data[], uint8_t length)
 bool dataReceive(uint8_t adress, uint8_t data[], uint8_t length){
 	bool result = true;
 	uint16_t config = NO_OF_VALUE|(PARTIAL_SCAN_00<<11)|(PARTIAL_SCAN<<13)|(CM_U<<14);
-	uint8_t configL = config&0xF;
-	uint8_t configH = (config&0xF0)>>8;
+	uint8_t configL = (uint8_t)(config&0x00FF);
+	uint8_t configH = (uint8_t)((config&0xFF00)>>8);
 	const headerInst_t inst1 = {{ACK_SYMBOL, STX_SYMBOL, ADR_SLV, LENGTH_LOW , LENGTH_HIGH, DATA_REQ_RESP,configL,configH}};
 	USART_set_Bytes_to_receive(iUSART0,0);
 	for (int i = 0;i<length;i++){
@@ -108,9 +128,9 @@ bool dataReceive(uint8_t adress, uint8_t data[], uint8_t length){
 		for (int i = HEADER_LENGTH;i<HEADER_LENGTH+722;i++){
 			filtered_data[i] = datastream[i];
 		}
-		distances = bytes_to_values(filtered_data);
-		rounded_distances = round_values(distances);
-		converted_distances = cm_to_m(rounded_distances);
+		distances = bytes_to_values((uint8_t *)filtered_data);
+		rounded_distances = round_values((uint8_t *)distances);
+		converted_distances = cm_to_m((uint8_t *)rounded_distances);
 		state = true;
 	}
 	USART_set_Bytes_to_receive(iUSART1,1);
@@ -137,7 +157,7 @@ bool daten_senden(uint8_t adress, uint8_t data[], uint8_t length)
 		//Pruefe Antwort vom Slave-Modul: 0xA1 = ACK; 0xA2 = NACK; und Anzahl der max. Sendeversuche
 		if ((data[0]==0xA2) && (num_of_send<3)) {
 			length = sizeof(converted_distances);
-			USART_send_Array(iUSART1 , 0, converted_distances, length);		//Wiederhole das Senden an Slave-Modul; maximal, aber bis zu 3 Versuche
+			USART_send_Array(iUSART1 , 0, (uint8_t *)converted_distances, length);		//Wiederhole das Senden an Slave-Modul; maximal, aber bis zu 3 Versuche
 			num_of_send++;											//Sendeversuchszaehler hochzaehlen
 		}
 		else {
@@ -158,7 +178,7 @@ bool dataSend(uint8_t adress, uint8_t data[], uint8_t length){
 *************************************************************/
 
 void setup() {
-	uint8_t start_command[] = {0x02, 0x80, 0x02, 0x00, 0x20, 0x24, 0x34, 0x08};
+	uint8_t start_command[] = {0x02, 0x00, 0x02, 0x00, 0x20, 0x24, 0x34, 0x08};
 	init_Core_CLK();
 	USART_init(iUSART0,BAUDRATE_SENSOR, USART_CHSIZE_8BIT_gc, USART_PMODE_DISABLED_gc, USART_SBMODE_1BIT_gc,false,0,0,PORTMUX_USARTx_DEFAULT_gc); //UART für Sensor
 	//USART_receive_Array_callback_fnc_t USART_receive_Array_callback_in = &daten_empfangen; //Initialisierung von einem Zeiger auf die Funktion zum Daten empfangen
@@ -172,15 +192,19 @@ void setup() {
 
 //refactoring of function setup()
 uint8_t init(){
-	uint8_t start_command = {0x02, 0x00, 0x02, 0x00, 0x20, 0x24, 0x34, 0x08};
+	uint8_t start_command_wth_checksum[]={STX_SYMBOL, SENSOR_ADR, 0x02, 0x00, SET_OP_MODE, ALL_VALUE_COUNTINUE};
+	uint16_t checksumValue = checksumCrc16(start_command_wth_checksum, sizeof(start_command_wth_checksum)/sizeof(uint8_t));
+	uint8_t checksumValueL = (uint8_t)(checksumValue & 0x00FF);
+	uint8_t checksumValueH = (uint8_t)(checksumValue & 0xFF00);
+	uint8_t start_command[] = {STX_SYMBOL, SENSOR_ADR, 0x02, 0x00, SET_OP_MODE, ALL_VALUE_COUNTINUE, checksumValueL, checksumValueH};
 	init_Core_CLK();
 	bool sensorUartInit = USART_init(iUSART0,BAUDRATE_SENSOR, USART_CHSIZE_8BIT_gc, USART_PMODE_DISABLED_gc, USART_SBMODE_1BIT_gc, SYNC_TX,MPC_MODE, 0, PORTMUX_USARTx_DEFAULT_gc);
 	USART_set_receive_Array_callback_fnc(iUSART0,&dataReceive);
 	bool slaveUartInit = USART_init(iUSART1,BAUDRATE_SLAVE, USART_CHSIZE_8BIT_gc, USART_PMODE_ODD_gc, USART_SBMODE_1BIT_gc, SYNC_TX, MPC_MODE, 0, PORTMUX_USARTx_DEFAULT_gc);
 	USART_set_receive_Array_callback_fnc(iUSART1,&dataSend);
 	USART_set_Bytes_to_receive(iUSART1,1);
-	USART_send_Array(iUSART0, SENSOR_ADR, start_command, sizeof(start_command));
-	return 0;
+	USART_send_Array(iUSART0, 0, start_command, sizeof(start_command));
+	return sensorUartInit&&slaveUartInit;
 }
 
 /*CRC8**********************************************************
@@ -222,9 +246,9 @@ int main(void) {
 		//wenn Sendestatus = true, dann bilde Checksumme und sende Daten an Slave
 		if (state) {
 			length = sizeof(converted_distances);
-			crc8 = CRC8(converted_distances, length);
+			crc8 = CRC8((uint8_t*)converted_distances, length);
 			converted_distances[length+1] = crc8;
-			USART_send_Array(iUSART1 , adress, converted_distances, length);
+			USART_send_Array(iUSART1 , adress, (uint8_t*)converted_distances, length);
 			if (ack) {
 				//Sendestatus zurücksetzen
 				state = false;
