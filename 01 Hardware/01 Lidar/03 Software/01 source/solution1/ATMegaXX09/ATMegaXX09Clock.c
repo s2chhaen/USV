df0585bfabaede10519acddbd730c7cb6e11f186
hathach23 <53267440+hathach23@users.noreply.github.com>
2023-06-20 16:10:02 +0200

    Refaktorisierung der Init-Funktion für Taktgeber

--- 01 Hardware/01 Lidar/03 Software/01 source/solution1/ATMegaXX09/ATMegaXX09Clock.c
+++ 01 Hardware/01 Lidar/03 Software/01 source/solution1/ATMegaXX09/ATMegaXX09Clock.c
@@ -42,39 +42,88 @@ void get_CPU_CLK(void)
 	}
 }
 
-
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 // init Core Clock with 20Mhz and start the external 32,768 Crystal Oscillator
 ////////////////////////////////////////////////////////////////////////////////////////////////////
-void init_Core_CLK(void)
+void init_Core_CLK(uint8_t type, uint8_t prescaler)
 {
 	register uint8_t reg_val = 0;
-	
-	// internen 20MHz Oszillator auswählen
-	if((CLKCTRL.MCLKSTATUS & CLKCTRL_OSC20MS_bm) == CLKCTRL_OSC20MS_bm)
-	{
-		CLKCTRL.MCLKCTRLA = CLKCTRL_CLKSEL_OSC20M_gc;
-		CLKCTRL.MCLKCTRLA &= ~CLKCTRL_CLKOUT_bm;
+	uint8_t prescaler_bm=0;
+	//Nur eine Art von Taktgeber darf gleichzeitig ausgewaehaelt werden
+	switch (prescaler){
+	case 1:
+		prescaler = ~CLKCTRL_PEN_bm;
+		break;
+	case 2:
+		prescaler = (CLKCTRL_PEN_bm)|(CLKCTRL_PDIV_2X_gc);
+		break;
+	case 4:
+		prescaler = (CLKCTRL_PEN_bm)|(CLKCTRL_PDIV_4X_gc);
+		prescaler = SET_PRESCALE(CLKCTRL_PEN_bm,4);
+		break;
+	case 6:
+		prescaler = (CLKCTRL_PEN_bm)|(CLKCTRL_PDIV_6X_gc);
+		break;
+	case 8:
+		prescaler = (CLKCTRL_PEN_bm)|(CLKCTRL_PDIV_8X_gc);
+		break;
+	case 10:
+		prescaler = (CLKCTRL_PEN_bm)|(CLKCTRL_PDIV_10X_gc);
+		break;
+	case 12:
+		prescaler = (CLKCTRL_PEN_bm)|(CLKCTRL_PDIV_12X_gc);
+		break;
+	case 16:
+		prescaler = (CLKCTRL_PEN_bm)|(CLKCTRL_PDIV_16X_gc);
+		break;
+	case 24:
+		prescaler = (CLKCTRL_PEN_bm)|(CLKCTRL_PDIV_24X_gc);
+		break;
+	case 32:
+		prescaler = (CLKCTRL_PEN_bm)|(CLKCTRL_PDIV_32X_gc);
+		break;
+	case 48:
+		prescaler = (CLKCTRL_PEN_bm)|(CLKCTRL_PDIV_48X_gc);
+		break;
+	case 64:
+		prescaler = (CLKCTRL_PEN_bm)|(CLKCTRL_PDIV_64X_gc);
+		break;
+	default:
+		break;
 	}
-	
-	// Kein Prescaler von CLK
-	reg_val = CLKCTRL.MCLKCTRLB;
-	reg_val &= ~CLKCTRL_PEN_bm;
-	CPU_CCP = CCP_IOREG_gc;
-	CLKCTRL.MCLKCTRLB = reg_val;
-	
-	get_CPU_CLK();
-	
-	
-	// externen 32.768 kHz Crystal Oscillator auswählen für RTC
-	reg_val = CLKCTRL.XOSC32KCTRLA;
-	reg_val = (reg_val & ~CLKCTRL_CSUT_gm) | CLKCTRL_CSUT_16K_gc;
-	reg_val |= CLKCTRL_RUNSTDBY_bm;
-	reg_val |= CLKCTRL_ENABLE_bm;
-	CPU_CCP = CCP_IOREG_gc;
-	CLKCTRL.XOSC32KCTRLA = reg_val;
-	
-	while((CLKCTRL.MCLKSTATUS & CLKCTRL_XOSC32KS_bm) != CLKCTRL_XOSC32KS_bm);
+	switch(type){
+		case EXTCLK:
+			// externen 32.768 kHz Crystal Oscillator auswählen für RTC
+			reg_val = CLKCTRL.XOSC32KCTRLA;
+			reg_val = (reg_val & ~CLKCTRL_CSUT_gm) | CLKCTRL_CSUT_16K_gc;
+			reg_val |= CLKCTRL_RUNSTDBY_bm;
+			reg_val |= CLKCTRL_ENABLE_bm;
+			CPU_CCP = CCP_IOREG_gc;
+			CLKCTRL.XOSC32KCTRLA = reg_val;
+			get_CPU_CLK();
+			while((CLKCTRL.MCLKSTATUS & CLKCTRL_XOSC32KS_bm) != CLKCTRL_XOSC32KS_bm);
+			break;
+		case INTERN_CLK:
+			//16/20MHz Internal oscillator
+			reg_val = CLKCTRL.MCLKCTRLA;
+			reg_val |= CLKCTRL_CLKSEL_OSC20M_gc;
+			reg_val &= ~CLKCTRL_CLKOUT_bm;
+			CCP=CCP_IOREG_gc;
+			CLKCTRL.MCLKCTRLA = reg_val;
+			
+			//Prescaler disable
+			reg_val = prescaler;
+			CCP=CCP_IOREG_gc;
+			CLKCTRL.MCLKCTRLB = reg_val;
+			//unlock
+			CCP=CCP_IOREG_gc;
+			CLKCTRL.MCLKLOCK &= ~CLKCTRL_LOCKEN_bm;
+			get_CPU_CLK();
+			while(CLKCTRL.MCLKSTATUS !=(CLKCTRL_OSC20MS_bm &(~CLKCTRL_EXTS_bm)&(~CLKCTRL_XOSC32KS_bm)&(~CLKCTRL_SOSC_bm)));
+			break;
+		default:
+			break;
+	}	
 }
 
 //EOF//
