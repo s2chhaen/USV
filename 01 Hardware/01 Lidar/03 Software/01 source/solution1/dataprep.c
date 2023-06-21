2cafa0c08c19e5668f7ebfb3414853a6a3952e2d
hathach23 <s8dathac@stud.eah-jena.de>
2023-06-21 17:47:36 +0200

    weitere Reafaktorisierung der Funktionen bytes_to_value(), round_values() und cm_to_m(). Noch zu erledigen

--- 01 Hardware/01 Lidar/03 Software/01 source/solution1/dataprep.c
+++ 01 Hardware/01 Lidar/03 Software/01 source/solution1/dataprep.c
@@ -13,15 +13,12 @@
 *gefilterte Daten bestehen aus 722 Byte in der Form (LSB, MSB) pro Datenpunkt
 *Werte werden zu Distanzen verrechnet, resultierendes Array enthält 361 Daten (0,5° je Messpunkt, 180° gesamt)
 *************************************************************/
-processVal_t bytes_to_values(uint8_t input[], uint16_t inLength, uint8_t output[], uint16_t outLength) {
-	processVal_t returnValue = NO_ERROR;
-#ifndef DEBUGGING
-	uint8_t rounded_data[361];
-#endif
+processResult_t bytes_to_values(uint8_t input[], uint16_t inLength, uint8_t output[], uint16_t outLength) {
+	processResult_t returnValue = NO_ERROR;
 	if ((input==NULL)||(output==NULL)){
 		returnValue = NULL_POINTER;
 	} else if(inLength>(outLength*2)){
-		returnValue = NOT_ENOUGH_OUTPUT_SPACE;
+		returnValue = IN_OUT_NOT_EQUAL;
 	} else{
 		for (int i=0; i<=inLength; i+=2) {
 			output[i/2] = input[i] + 256*input[i+1];
@@ -39,11 +36,8 @@ processVal_t bytes_to_values(uint8_t input[], uint16_t inLength, uint8_t output[
 *gemäß Forderung werden die Messwerte (liegen in cm vor) auf 50cm genau gerundet
 *aus Sicherheitsgründen: immer abrunden
 *************************************************************/
-processVal_t round_values(uint8_t data[],uint16_t length) {
-	processVal_t returnValue = NO_ERROR;
-#ifndef DEBUGGING
-	uint8_t rounded_data[361];
-#endif
+processResult_t round_values(uint8_t data[],uint16_t length) {
+	processResult_t returnValue = NO_ERROR;
 	if (data!=NULL){
 		for (int i=0; i<=length; i++) {
 			/*abrunden wie folgt: 3412cm --> 3000cm, 2976cm --> 2500cm, 891cm --> 500cm*/
@@ -64,11 +58,8 @@ processVal_t round_values(uint8_t data[],uint16_t length) {
 *Funktion konvertiert die Messwerte (in cm) zu m
 *gemäß Forderung werden die Daten mit 2 multipliziert, um ganzzahlige Werte an Slave senden zu können
 *************************************************************/
-processVal_t cm_to_m (uint8_t data[],uint16_t length) {
-	processVal_t returnValue = NO_ERROR;
-#ifndef DEBUGGING
-	uint8_t converted_data[361];
-#endif
+processResult_t cm_to_m (uint8_t data[],uint16_t length) {
+	processResult_t returnValue = NO_ERROR;
 	for (int i=0; i<=length; i++) {
 		//Abrunden wie folgt: kleiner als 0,5 zu 0,5 und sonst zu 1
 		data[i] = data[i]*2/100;
--- 01 Hardware/01 Lidar/03 Software/01 source/solution1/dataprep.h
+++ 01 Hardware/01 Lidar/03 Software/01 source/solution1/dataprep.h
@@ -1,12 +1,12 @@
 #ifndef DATAPREP_H_
 #define DATAPREP_H_
 
-#define DEBUGGING 1
+#include "error_list.h"
 
-extern processVal_t bytes_to_values(uint8_t input[], uint16_t inLength, uint8_t output[], uint16_t outLength);
-extern processVal_t round_values(uint8_t data[],uint16_t length);
-extern processVal_t cm_to_m (uint8_t data[],uint16_t length);
+extern processResult_t bytes_to_values(uint8_t input[], uint16_t inLength, uint8_t output[], uint16_t outLength);
+extern processResult_t round_values(uint8_t data[],uint16_t length);
+extern processResult_t cm_to_m (uint8_t data[],uint16_t length);
 
-typedef enum {NO_ERROR,NULL_POINTER,NOT_ENOUGH_OUTPUT_SPACE}processVal_t;
+
 
 #endif
