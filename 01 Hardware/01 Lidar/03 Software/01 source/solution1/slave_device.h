38de1f52d9351126bd24ab11a5d3a94bbeae95e4
hathach23 <thach.hoangha23@gmail.com>
2023-06-23 14:32:46 +0200

    machen callbackTx() und callbackRx() lokale Funktionen

--- 01 Hardware/01 Lidar/03 Software/01 source/solution1/slave_device.h
+++ 01 Hardware/01 Lidar/03 Software/01 source/solution1/slave_device.h
@@ -10,25 +10,53 @@
 #ifndef SLAVE_H_
 #define SLAVE_H_
 
-#include "error_list.h"
-#include "ATMegaXX09/USART/USART.h"
 #include <stdint.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <avr/interrupt.h>
+#include "lock.h"
+#include "error_list.h"
+#include "ATMegaXX09/USART/USART.h"
  
+ //#define SLAVE_STATIC 0
+
+#define MAX_BYTE_SEND 16
+
+#ifdef SLAVE_DYNAMIC
 typedef struct {
 	uint8_t uart;
 	volatile uint16_t txTime;
-	//volatile uint16_t toTxByte;
+	volatile uint16_t toTxByte;
 	volatile uint16_t rxTime;
-	//volatile uint16_t toRxByte;
-	uint16_t rxLenMax;
+	volatile uint16_t toRxByte;
+	
 	uint16_t txLenMax;
 	volatile uint8_t *txBuffer;
+	volatile uint16_t txPtrPosition;
+	uint16_t rxLenMax;
 	volatile uint8_t *rxBuffer;
+	//volatile uint16_t rxPtrPosition;
 	uint8_t initState:1;
 	uint8_t crcActive:1;
 }slaveDevice_t;
+#else
+typedef struct {
+	uint8_t uart;
+	volatile uint16_t txTime;
+	volatile uint16_t toTxByte;
+	volatile uint16_t rxTime;
+	volatile uint16_t toRxByte;
+	
+	uint16_t txLenMax;
+	volatile uint8_t txBuffer[900];
+	volatile uint16_t txPtrPosition;
+	uint16_t rxLenMax;
+	volatile uint8_t rxBuffer[900];
+	//volatile uint16_t rxPtrPosition;
+	uint8_t initState:1;
+	uint8_t crcActive:1;
+}slaveDevice_t;
+#endif
 
 typedef enum {
 	ON,
@@ -39,11 +67,7 @@ extern processResult_t initDev(uint16_t rxLength, uint16_t txLength,uint8_t USAR
 					USART_CHSIZE_t bits, USART_PMODE_t parity,USART_SBMODE_t stopbit, \
 					bool sync, bool MPCM, uint8_t address, PORTMUX_USARTx_t PortMux);
 
-extern processResult_t dataTx(uint16_t length, uint8_t* data);
+extern processResult_t dataTx(uint8_t* data, uint16_t length);
 extern processResult_t dataRx(uint16_t length, uint8_t* data);
-
-extern bool callbackRx(uint8_t adress, uint8_t data[], uint8_t length);
-extern bool callbackTx(uint8_t* adress, uint8_t* data[], uint8_t* length,uint8_t max_length);
-
 
 #endif /* SLAVE_H_ */
