617627d4c5bf5d911dcc9d36047cea60704b3944
hathach23 <thach.hoangha23@gmail.com>
2023-07-03 23:38:39 +0200

    Hinzugefuegt der Konfiguration-Header-Datei fuer Slave-Device

---
+++ 01 Hardware/01 Lidar/03 Software/01 source/solution1/slaveDeviceConfig.h
@@ -0,0 +1,19 @@
+/*
+ * slaveDeviceConfig.h
+ *
+ * Created: 7/3/2023 10:04:33 PM
+ *  Author: Dan
+ */ 
+
+
+#ifndef SLAVEDEVICECONFIG_H_
+#define SLAVEDEVICECONFIG_H_
+
+//#define SLAVE_STATIC 0
+#define RX_BUFFER_LEN 900
+#define TX_BUFFER_LEN 900
+#define NO_OF_RX_BUFFER 4
+
+
+
+#endif /* SLAVEDEVICECONFIG_H_ */
