01d6b50f7b30c33c0bc9c73183e35a67eb7d4252
hathach23 <thach.hoangha23@gmail.com>
2023-07-04 16:25:25 +0200

    neue Definition fuer FIFO-Zustand

--- 01 Hardware/01 Lidar/03 Software/01 source/solution1/slaveDeviceConfig.h
+++ 01 Hardware/01 Lidar/03 Software/01 source/solution1/slaveDeviceConfig.h
@@ -10,10 +10,15 @@
 #define SLAVEDEVICECONFIG_H_
 
 //#define SLAVE_STATIC 0
+#define FULL_STRUCT 0
 #define RX_BUFFER_LEN 900
 #define TX_BUFFER_LEN 900
 #define NO_OF_RX_BUFFER 4
 
-
+enum fifoState{
+	EMPTY,
+	FILLED,
+	FULL
+};
 
 #endif /* SLAVEDEVICECONFIG_H_ */
