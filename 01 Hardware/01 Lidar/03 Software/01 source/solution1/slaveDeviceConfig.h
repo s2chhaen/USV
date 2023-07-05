c034aa8155ca89fa94ad61e1884599b5a345a218
hathach23 <thach.hoangha23@gmail.com>
2023-07-06 00:50:10 +0200

    der Zustand von rx-FIFO und die Parameter fuer slaveDevice wieder defniert

--- 01 Hardware/01 Lidar/03 Software/01 source/solution1/slaveDeviceConfig.h
+++ 01 Hardware/01 Lidar/03 Software/01 source/solution1/slaveDeviceConfig.h
@@ -9,14 +9,17 @@
 #ifndef SLAVEDEVICECONFIG_H_
 #define SLAVEDEVICECONFIG_H_
 
-//#define SLAVE_STATIC 0
-#define FULL_STRUCT 0
-#define RX_BUFFER_LEN 900
-#define TX_BUFFER_LEN 900
-#define NO_OF_RX_BUFFER 4
+#define MAX_BYTE_SEND 31
+
+#define RX_BUFFER_LEN 800
+#define TX_BUFFER_LEN 100
+#define NO_OF_RX_BUFFER 2
+#define USART_TIME_PRO_BYTE_US 96
+
+#define ACTIVE_USART_WATCHER 1
 
 enum fifoState{
-	EMPTY,
+	EMPTY=0,
 	FILLED,
 	FULL
 };
