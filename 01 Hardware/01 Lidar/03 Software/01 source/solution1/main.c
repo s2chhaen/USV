d0678222b1faaff0ce533d6d022513c88bef14a6
hathach23 <thach.hoangha23@gmail.com>
2023-07-06 16:40:51 +0200

    nicht notwendige Variablen und Funktionen loeschen. Das Test Programm fuer Inbetriebnahme von USART hinzugefuegt, getestet, funktioniert gut in Baudrate von 76,8kBaud

--- 01 Hardware/01 Lidar/03 Software/01 source/solution1/main.c
+++ 01 Hardware/01 Lidar/03 Software/01 source/solution1/main.c
@@ -2,41 +2,11 @@
  * solution1.c
  *
  * Created: 11.03.2023 16:31:29
- * Author : Riccardo
- * Bearbeiter: Thach
+ * Author : Thach
  */ 
 
 
 #include "main.h"
-
-/* Header vor dem Senden der eigentlichen Daten: 06 02 80 D6 02 B0 69 01 
-anschließend folgen 722 Datenbyte --> 361 Datenpunkte, jeweils LSB / MSB
-
-UART RS232 - ATMega: iUSART0
-UART Slave - ATMega: iUSART1
-UART RS422 - ATMega: iUSART2
-
-*/
-
-/*Globale Variablen**********************************************************
-*uint8_t ack : wird mit Acknowledge Antwort vom Slave überschrieben
-*uint8_t *datastream : enthält die vom USART empfangenen Sensordaten
-*uint8_t *filtered_data : enthält die gefilterten Sensordaten (nach Finden des Headers)
-*uint8_t *distances : enthält die berechneten Distanzen in cm
-*uint8_t *rounded_distances : enthält die auf 0,5m gerundeten Distanzen
-*uint8_t *converted_distances : enthält die von cm in m konvertierten Distanzen gemäß Forderung
-*bool state = false : Abarbeitungsstatus der empfangenen Daten; ist true, wenn Daten gesendet werden können
-*************************************************************/
-volatile uint8_t ack = 0x0;
-volatile uint8_t datastream[MAX_FRAME_LENGTH] = {0};
-volatile uint8_t filtered_data[DATA_STREAM_SIZE] = {0};
-volatile uint8_t distances[DATA_STREAM_SIZE] = {0};
-volatile uint8_t rounded_distances[DATA_STREAM_SIZE] = {0};
-volatile uint8_t converted_distances[DATA_STREAM_SIZE] = {0};
-volatile bool state = false;
-volatile uint16_t counter = 0;
-static uint16_t txDataLength = 0;
-
 
 //Implementation von CRC16-Checksum durch Anpassung Muster-Program in Buch von Hersteller
 uint16_t checksumCrc16(uint8_t input[], uint32_t length){
@@ -78,91 +48,57 @@ uint8_t CRC8( uint8_t *addr, uint8_t len){
 	return crc;
 }
 
-//refactoring of function daten_empfangen
-bool dataReceive(uint8_t adress, uint8_t data[], uint8_t length){
-	bool result = true;
-	/**
-	uint16_t config = NO_OF_VALUE|(PARTIAL_SCAN_00<<11)|(PARTIAL_SCAN<<13)|(CM_U<<14);
-	uint8_t configL = (uint8_t)(config&0x00FF);
-	uint8_t configH = (uint8_t)((config&0xFF00)>>8);
-	const headerInst_t inst1 = {{ACK_SYMBOL, STX_SYMBOL, ADR_SLV, LENGTH_LOW , LENGTH_HIGH, DATA_REQ_RESP,configL,configH}};
-	USART_set_Bytes_to_receive(iUSART0,0);
-	for (int i = 0;i<length;i++){
-		datastream[i] = data[i];
-	}
-
-	for (int i = 0;i<HEADER_LENGTH;i++){
-		if (datastream[i]!=inst1.data[i]){
-			result = false;
-			break;
-		}
-	}
-	if(result){
-		for (int i = HEADER_LENGTH;i<HEADER_LENGTH+722;i++){
-			filtered_data[i] = datastream[i];
-		}
-		//processVal_t byteConvert = bytes_to_values((uint8_t *)filtered_data,MAX_FRAME_LENGTH,(uint8_t *)distances,DATA_STREAM_SIZE);
-		//processVal_t roundValue = round_values((uint8_t *)distances,DATA_STREAM_SIZE);
-		//processVal_t distancesConvert = cm_to_m((uint8_t *)rounded_distances,DATA_STREAM_SIZE);
-		state = true;
-	}
-	USART_set_Bytes_to_receive(iUSART1,1);
-	******/
-	return result;
-}
-
-bool dataSend(uint8_t adress, uint8_t data[], uint8_t length){
-	//static uint8_t counter=3;
-	bool result = false;
-	/***
-	if((data[0]==NACK_SYMBOL)&&(counter)){
-		length = sizeof(converted_distances);
-		result = USART_send_Array(iUSART1 , 0, (uint8_t *)converted_distances, length);
-		counter--;
-	} else{
-		ack = (data[0]==ACK_SYMBOL)?1:0;
-		counter = 3;
-	}
-	*****/
-	counter--;
-	return result;
-}
-
-//refactoring of function setup()
 uint8_t init(){
 	uint8_t result;
 	uint8_t prescaler = 1;
 	uint16_t rxLength = 900;
 	uint16_t txLength = 900;
-	//uint32_t baudrateSlave = 250000;
-	uint32_t baudrateSlave = 115200;
+	uint8_t timerResUs = 1;
+	uint16_t timerPre = 1024;
+	uint32_t baudrateSlave = 76800;
+	//CPU-Init
 	init_Core_CLK(INTERN_CLK,prescaler);
-	//result = USART_init(iUSART1,250000, USART_CHSIZE_8BIT_gc, USART_PMODE_ODD_gc, USART_SBMODE_1BIT_gc, DISBL_SYNC_TX, DISBL_MPC_MODE, 0, PORTMUX_USARTx_DEFAULT_gc);
+	//USART-Beobachter- und Stopuhr-Einheit-Init
+	timerInit(timerResUs,timerPre);
+	//Slave-Device-Init
 	result = initDev(rxLength, txLength, iUSART1, baudrateSlave, USART_CHSIZE_8BIT_gc, USART_PMODE_ODD_gc, USART_SBMODE_1BIT_gc, DISBL_MPC_MODE, DISBL_MPC_MODE, 0, PORTMUX_USARTx_DEFAULT_gc);
-	sei();//active the global interrupt, weil es bisher nicht aktiviert wird
+	sei();//aktiviert das global Interrupt, weil es bisher nicht aktiviert wird
 	return result;
 }
 
+#ifdef ACTIVE_ECHO_TEST
+#define DATA_LENGTH 100
+static uint8_t data[DATA_LENGTH]={0};
+static uint16_t rxLength = sizeof(data)/sizeof(uint8_t);
+static void echoTest(){
+	volatile bool checkReceive = dataRx((uint8_t*)data,(uint16_t*)&rxLength)==NO_ERROR;
+	volatile uint16_t temp1 = 48;
+	volatile uint16_t temp2 = 48;
+	if(checkReceive){
+		dataTx((uint8_t*)data,(uint16_t)rxLength);
+		waitUs(1000);
+		temp1+=rxLength/10;
+		temp2+=rxLength%10;
+		data[0]=temp1;
+		data[1]=temp2;
+		dataTx((uint8_t*)data,2);
+		rxLength = sizeof(data)/sizeof(uint8_t);
+		memset((uint8_t*)data,0,rxLength);
+	}
+}
+#endif
+
 int main(void) {
-#ifdef DATA_FOR_TEST
+	init();
+#ifdef ACTIVE_HAL_USART_TEST_SEG_1
 #define MAX_VALUE_TEST 31
 	uint8_t test[MAX_VALUE_TEST]={0};
 	for (int i = 0; i < MAX_VALUE_TEST; i++){
 		test[i] = i;
 	}
-	txDataLength = sizeof(test)/sizeof(uint8_t);
-#endif
-
-	init();
-
-#ifdef ACTIVE_HAL_USART_TEST_SEG_1
-for (uint8_t u = 0;u<10;u++)
-{
+	uint8_t txDataLength= sizeof(test)/sizeof(uint8_t);
 	USART_send_Array(iUSART1, 0x0, test, txDataLength);
-	for (uint32_t i = 0; i<30000;i++){}//warte 30 000 cycles
-}
-#endif	
-
+#endif
 #ifdef ACTIVE_HAL_USART_TEST_SEG_2
 #define MAX_LENGTH_BUFFER 50
 	USART_set_Bytes_to_receive(iUSART1,31);
@@ -175,10 +111,18 @@ for (uint8_t u = 0;u<10;u++)
 #endif
 		
 #ifdef ACTIVE_SLV_DEVICE_SEND_TEST_SEG
+	#define MAX_VALUE_TEST 31
+	uint8_t test[MAX_VALUE_TEST]={0};
+	for (int i = 0; i < MAX_VALUE_TEST; i++){
+		test[i] = i;
+	}
+	uint8_t txDataLength= sizeof(test)/sizeof(uint8_t);
 	dataTx(test,txDataLength);
 #endif
 	volatile uint8_t ix = 0;
+#ifdef ACTIVE_HAL_USART_TEST_SEG_2
 	volatile bool checkRx = false;
+#endif
     while (1){
 		ix++;
 #ifdef ACTIVE_HAL_USART_TEST_SEG_2
@@ -186,6 +130,9 @@ for (uint8_t u = 0;u<10;u++)
 		if(rxLength!=0){
 			USART_send_Array(iUSART1, 0x0, rxBuffer,rxLength);
 		}
+#endif
+#ifdef ACTIVE_ECHO_TEST
+		echoTest();
 #endif
     }
 	return 0;
