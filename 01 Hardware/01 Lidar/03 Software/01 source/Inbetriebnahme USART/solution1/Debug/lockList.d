3b2165f1a36c36b6a6afdca40835cfde7ea9c13c
hathach23 <thach.hoangha23@gmail.com>
2023-07-07 04:14:13 +0200

    die nicht notwendigen Daten werden geloest

--- 01 Hardware/01 Lidar/03 Software/01 source/Inbetriebnahme USART/solution1/Debug/lockList.d
+++
@@ -1,9 +0,0 @@
-lockList.d lockList.o: .././lockList.c .././lock.h \
- c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdint.h \
- c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h
-
-.././lock.h:
-
-c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdint.h:
-
-c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h:
--- 01 Hardware/01 Lidar/03 Software/01 source/Inbetriebnahme USART/solution1/Debug/mutex.d
+++
@@ -1,9 +0,0 @@
-mutex.d mutex.o: .././mutex.c .././mutex.h \
- c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdint.h \
- c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h
-
-.././mutex.h:
-
-c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdint.h:
-
-c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h:
