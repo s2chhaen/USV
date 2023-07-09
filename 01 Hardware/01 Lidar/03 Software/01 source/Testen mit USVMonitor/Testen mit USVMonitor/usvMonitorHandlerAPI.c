40d75c8a226488ab4178b4000f2094a936bf97e4
hathach23 <thach.hoangha23@gmail.com>
2023-07-09 17:25:56 +0200

    initDev() refaktorisiert und dokumentiert: neue Initialisation entsprechenden dem neuen redefinierten usvMonitorHandler_t Struct

--- 01 Hardware/01 Lidar/03 Software/01 source/Testen mit USVMonitor/Testen mit USVMonitor/usvMonitorHandlerAPI.c
+++ 01 Hardware/01 Lidar/03 Software/01 source/Testen mit USVMonitor/Testen mit USVMonitor/usvMonitorHandlerAPI.c
@@ -1,5 +1,5 @@
 /*
- * usvMonitor.c
+ * usvMonitorHandlerAPI.c
  *
  * Created: 7/7/2023 8:42:59 AM
  * Author: Thach
@@ -42,26 +42,48 @@ static inline int8_t searchReg(uint16_t reg){
 	return result;
 }
 
-
-/**
- * @brief Initalisierung des Handlers
- * 
- * @param dev_p der Zeiger zum betrachteten usVMonitorHandler
- * @param config_p der Zeiger zur erwuenschten USART-Konfiguration
- * 
- * @return uint8_t 0: erfolgreich, sonst: nicht
- */
-uint8_t initDev(usvMonitorHandler_t* dev_p, usvMonitorConfig_t* config_p){
-	uint8_t result = NO_ERROR;
-	dev_p->statusObj.uart = config_p->usartNo;
-	bool devInit = USART_init(config_p->usartNo,config_p->baudrate, config_p->bits, config_p->parity, config_p->stopbit, config_p->sync, config_p->MPCM,config_p->address, config_p->PortMux);
-	USART_set_send_Array_callback_fnc(dev_p->statusObj.uart,config_p->tx_p);
-	USART_set_Bytes_to_receive(dev_p->statusObj.uart,MAX_BYTE_SEND);
-	USART_set_receive_Array_callback_fnc(dev_p->statusObj.uart,config_p->rx_p);
-	dev_p->statusObj.initState = (!devInit)?1:0;
-	result = (!devInit)?NO_ERROR:PROCESS_FAIL;
+//keine Fehler protocol ausgeben, man muss die Gueltigkeit checken
+static inline uuaslReadProtocol_t readProtocolPrint(uint8_t add,uint16_t index){
+	uuaslReadProtocol_t result ={
+		.header.start =	0xA5,
+		.header.slaveRegAdd = regSet[index].add,
+		.header.rwaBytes.value_bf.slaveAddH = GET_SLAVE_ADD_HIGH_PART(add),
+		.header.rwaBytes.value_bf.slaveAddL = GET_SLAVE_ADD_LOW_PART(add),
+		.header.rwaBytes.value_bf.rw = UUASL_R_REQ,
+		.header.length = 8,
+		.dataLen = regSet[index].len,
+		.tail.end = 0xA6
+	};
+	result.tail.checksum = crc8(&(result.dataLen), sizeof(result.dataLen), CRC8_POLYNOM);
 	return result;
 }
+
+
+/**
+ * \brief Initalisierung des Handlers
+ * 
+ * \param dev_p Zeiger zum zu initalisierenden Handler 
+ * \param inputRxFunc_p der Zeiger zur Datenempfangen Funktion
+ * \param inputTxFunc_p der Zeiger zur Datensenden Funktion
+ * \param inputWaitFunc_p der Zeiger zur Warte Funktion
+ * \param inputCrc8 der Checksum-CRC8 Polynom
+ * 
+ * \return uint8_t 0: keinen Fehler, sonst Fehler
+ */
+uint8_t initDev(usvMonitorHandler_t* dev_p, dataRx_t inputRxFunc_p, dataTx_t inputTxFunc_p, wait_t inputWaitFunc_p, uint8_t inputCrc8){
+	uint8_t result = NO_ERROR;
+	if ((inputRxFunc_p!=NULL)&&(inputTxFunc_p!=NULL)&&(inputWaitFunc_p!=NULL)){
+		dev_p->receiveFunc_p = inputRxFunc_p;
+		dev_p->transmitFunc_p = inputTxFunc_p;
+		dev_p->waitFunc_p = inputWaitFunc_p;
+		dev_p->crc8Polynom = inputCrc8;
+	} else{
+		result = NULL_POINTER;
+	}
+	return result;
+	
+}
+
 
 uint8_t setData(uint8_t add, uint16_t reg, usvMonitorHandler_t* dev){
 	uint8_t result = NO_ERROR;
@@ -75,42 +97,51 @@ uint8_t setData(uint8_t add, uint16_t reg, usvMonitorHandler_t* dev){
 	return result;
 }
 
-uint8_t getData(uint8_t add, uint16_t reg, usvMonitorHandler_t* dev){
+//empfangen die Daten aus dem Slave-Gerät, inklusiv CRC-Byte
+uint8_t getData(uint8_t add, uint16_t reg, usvMonitorHandler_t* dev, uint8_t* output, uint16_t outputLen){
 	uint8_t result = NO_ERROR;
-	int8_t index = searchReg(reg);
-	if (index!=-1){
-		uuaslReadProtocol protocol={
-			.header.start =	0xA5,
-			.header.slaveRegAdd = regSet[index].add,
-			.header.rwaBytes.value_bf.slaveAddH = GET_SLAVE_ADD_HIGH_PART(add),
-			.header.rwaBytes.value_bf.slaveAddL = GET_SLAVE_ADD_LOW_PART(add),
-			.header.rwaBytes.value_bf.rw = UUASL_R_REQ,
-			.header.length = 8,
-			.dataLen = regSet[index].len,
-			.tail.end = 0xA6
-		};
-		protocol.tail.checksum = crc8(&(protocol.dataLen), sizeof(protocol.dataLen), CRC8_POLYNOM);
-		//Bildung von Datenrahmen
-		(*(dev->transmitFunc_p))((uint8_t*)&protocol, sizeof(protocol)/sizeof(uint8_t));
-		(*(dev->waitFunc_p))(10000);//Warte 10ms TODO: Implementation der Warte als dynamische Warte statt statische Warte
-		uint8_t rxBuffer[MAX_SIZE_FRAME]={0};
-		uint16_t rxLength=1;
-		(*(dev->receiveFunc_p))(rxBuffer, &rxLength);
-		if(rxBuffer[0]==0xA2){
-			result = DATA_INVALID;
-		} else{
-			rxLength=5;
-			uint16_t temp = ;
-			(*(dev->receiveFunc_p))(rxBuffer, &rxLength);
-			bool checkData = (rxBuffer[0]==0xA5) && \
-							(rxBuffer[1]==protocol.header.slaveRegAdd) && \
-							1 && \
-							1;//TODO mache noch weiter
-		}
-		//TODO zu handeln ist 2 Fälle: Fehler bei der Daten-Kosumer Seite (User-Unit), falsche bei der Daten-Anbieter Seite (Slave Seite)
-		//TODO Lösung: bei der Kosummer Seite: Fehler zurückgeben und bei der Anbieter Seite: wieder senden oder Fehler zurückgeben
+	if((dev==NULL)||(output==NULL)){
+		result = NULL_POINTER;
 	} else{
-		result = DATA_INVALID;
+		int8_t index = searchReg(reg);
+		if (index!=-1){
+			//Bildung von Datenrahmen
+			uuaslReadProtocol_t protocol = readProtocolPrint(add,index);			
+			(*(dev->transmitFunc_p))((uint8_t*)&protocol, sizeof(protocol)/sizeof(uint8_t));
+			(*(dev->waitFunc_p))(100);//Warte 0,1ms
+			uint8_t rxBuffer[MAX_SIZE_FRAME]={0};
+			uint16_t rxLength=1;
+			//Erst wird ein Byte empfangen lassen
+			(*(dev->receiveFunc_p))(rxBuffer, &rxLength);
+			if(rxBuffer[0]==0xA2){
+				result = DATA_INVALID;
+			} else{ //Wenn erfolgreich, dann 5 erste Bytes empfangen lassen und checken
+				rxLength=5;
+				(*(dev->receiveFunc_p))(rxBuffer, &rxLength);
+				(*(dev->waitFunc_p))(300);//warte 0,3ms
+				//Daten checken
+				//Byte 3 beim Daten lesen: Bei Hinprotokoll 0x4X, bei Rückprotokoll 0x0X => 0x4X XOR 0x0X = 0x40
+				bool checkData = (rxBuffer[0]==0xA5) && (rxBuffer[1]==protocol.header.slaveRegAdd) && \
+								((rxBuffer[2]^protocol.header.rwaBytes.value[0])==0x40) && \
+								(rxBuffer[3]==protocol.header.rwaBytes.value[1]);
+				if(!checkData){
+					result = PROCESS_FAIL;
+				} else{
+					//empfangen weiter n Byte und noch CRC- und Endbyte
+					rxLength = rxBuffer[4]-7+2;//offset im Register = 7
+					(*(dev->receiveFunc_p))(rxBuffer, &rxLength);
+					(*(dev->waitFunc_p))(300);
+					if (rxBuffer[rxLength-1]==0xA6){
+						rxLength--;
+						memcpy(output,rxBuffer,rxLength);
+					} else {
+						result = PROCESS_FAIL;
+					}
+				}
+			}
+		} else{
+			result = DATA_INVALID;
+		}
 	}
 	return result;
 }
