24f451d9daa36d75b6bdc052a3b6b4ab93f75839
hathach23 <thach.hoangha23@gmail.com>
2023-07-20 21:29:34 +0200

    Wiederanodrnung der Versuche in main() Funktion

--- 01 Hardware/01 Lidar/03 Software/01 source/Testen mit USVMonitor/Testen mit USVMonitor/main.c
+++ 01 Hardware/01 Lidar/03 Software/01 source/Testen mit USVMonitor/Testen mit USVMonitor/main.c
@@ -33,7 +33,6 @@ static void setErr1State(uint8_t state){
 	};
 }
 
-volatile uint8_t answer[9]={0};
 volatile uint8_t output[500]={0};
 	
 int main(void)
@@ -65,10 +64,8 @@ int main(void)
 	};
 	initUserUnit(config);
 	setErr1State(OFF);
-	//waitUs(100);
 	//User-Unit and Slave API Handler Init
 	usvMonitorHandler_t handler;
-	//waitCycle
 	initDev(&handler,usartDataRx,usartDataTx,waitCycle,0xD5);
 	sei();//globales Interrupt aktiviert
 	
@@ -81,108 +78,63 @@ int main(void)
 	setErr1State(ON);
 #endif	
 	
-#define TEST01 1
-	
-#ifdef TEST01
 	volatile uint8_t error1 = NO_ERROR;
-	//Lesen in Registern
 	const uint8_t add = 1;
 	uint16_t reg = 0;
 	uint8_t rxLen = 0;
 	
-	//reg = SEN_COURSE_ANGLE_ADD;
-	//rxLen = 2;
-	//reg = SEN_GESB_ADD;
-	//rxLen = 1;
-	//error1 = getData(add,reg,&handler,output,rxLen);
-	//waitUs(5);
-	//error1 = TIME_OUT;
+#define TEST01 1
+
+#ifdef TEST01
+	//Lesen in Registern
+	reg = SEN_COURSE_ANGLE_ADD;
+	rxLen = 2;
+	error1 = getData(add,reg,&handler,(uint8_t*)output,rxLen);
+	waitUs(5);
 	if (error1!=NO_ERROR){
 		setErr1State(ON);
-		//Warte 1min
-		for (int i = 0;i<60;i++){
-			waitUs(1000000);
-		}
-		setErr1State(OFF);
 	} else{
 		setErr1State(OFF);
-		//reg = SEN_LONGNITUDE_ADD;
-		//rxLen = 4;
-		//getData(add,reg,&handler,output,rxLen);
-		//waitUs(5);
-		//reg = SEN_LATITUDE_ADD;
-		//rxLen = 4;
-		//getData(add,reg,&handler,output,rxLen);
-		//waitUs(5);
-		//reg = SEN_SATFIX_ADD;
-		//rxLen = 1;
-		//getData(add,reg,&handler,output,rxLen);
-		//waitUs(5);
-		//reg = SEN_GPS_VEL_ADD;
-		//rxLen = 2;
-		//getData(add,reg,&handler,output,rxLen);
-		//waitUs(5);
-		//reg = SEN_COURSE_ANGLE_ADD;
-		//rxLen = 2;
-		//getData(add,reg,&handler,output,rxLen);
-		//waitUs(5);
-		//reg = SEN_TIMESTAMP_ADD;
-		//rxLen = 3;
-		//getData(add,reg,&handler,output,rxLen);
-		//waitUs(5);
-		//reg = SEN_GESB_ADD;
-		//rxLen= 1;
-		//getData(add,reg,&handler,output,rxLen);
-		//waitUs(5);
 	}
-	//Schreiben in Registern
-	//uint8_t input[20]={1,2};
-	//uint8_t txLen = 2;
-	//reg = REF_DRV_CTRL_VEL_ADD;
-	//error1 = setData(add,reg,&handler,input,txLen);
-	//if (error1!=NO_ERROR){
-		//setErr1State(ON);
-		//for (int i = 0;i<10;i++){
-			//waitUs(1000000);
-		//}
-		
-		//setErr1State(OFF);
-	//}
-	//reg = SEN_GESB_ADD;
-	//rxLen = 17;
+#endif
+
+#define TEST02 1
+
+#ifdef TEST02
+	//Schreiben in einem Register
+	uint8_t input[]={0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
+	reg = REF_DRV_CTRL_REF_A_ADD;	
+	rxLen = 8;
+	error1 = setData(add,reg,&handler,(uint8_t*)input, rxLen);
+	if (error1!=NO_ERROR){
+		setErr1State(ON);
+	} else {
+		setErr1State(OFF);
+	}
+#endif
+
+//#define TEST03 1
+
+#ifdef TEST03
+	//Multiregister schreiben und lesen
+	uint8_t input[]={0,1,2,3,4,5,6,7,8,9,10,11,12,14,15,16,17,18,19,20};
 	reg = REF_DRV_CTRL_REF_A_ADD;
-	rxLen = 21;
+	rxLen = 20;
+	error1 = setMultiregister(add,reg,&handler,(uint8_t*)input, rxLen);
+	if (error1!=NO_ERROR){
+		setErr1State(ON);
+	} else {
+		setErr1State(OFF);
+	}
 	error1 = getMultiregister(add,reg,&handler,(uint8_t*)output, rxLen);
 	if (error1!=NO_ERROR){
 		setErr1State(ON);
 	} else {
 		setErr1State(OFF);
 	}
-	
-#endif
-
-//#define TEST02 1
-
-#ifdef TEST02
-//#define LENGTH_TEST 62
-	//uint8_t test1[LENGTH_TEST]={0};
-	//for (uint8_t i = 0; i<LENGTH_TEST;i++){
-		//test1[i] = i;
-	//}
-	uint8_t dataSet[]={0xA5,0x01,0x0C,0x40,0x08,0x02,0x7F,0xA6};
-	//uint8_t dataSet[]={0xA5,0x00,0x0C,0x40,0x08,0x02, 0x7F,0xA6};
-	uint8_t length = sizeof(dataSet)/sizeof(uint8_t);
-	
-	(*(handler.transmitFunc_p))(dataSet,length);
-	waitUs(800);
-	length = 9;
-	(*(handler.receiveFunc_p))((uint8_t*)answer,length);
-	//USART_send_Array(iUSART1,0,test1,length);
-	uint8_t test111 = 0;
 #endif
     /* Replace with your application code */
     while (1){
-		//test111++;
     }
 }
 
