36a611486c440f302b72ccb7b2d4272b634cad24
hathach23 <thach.hoangha23@gmail.com>
2023-07-20 11:28:39 +0200

    Redefinition der maximalen Länge des Datenframes

--- 01 Hardware/01 Lidar/03 Software/01 source/Testen mit USVMonitor/Testen mit USVMonitor/usvMonitorHandlerAPI.h
+++ 01 Hardware/01 Lidar/03 Software/01 source/Testen mit USVMonitor/Testen mit USVMonitor/usvMonitorHandlerAPI.h
@@ -19,7 +19,7 @@
 #define GET_SLAVE_ADD_HIGH_PART(a) a>>8
 #define SET_SLAVE_ADD_LOW_PART(add) (uint8_t)(add&0xff)
 #define SET_SLAVE_ADD_HIGH_PART(add,rw) (uint8_t)((add>>8)|(rw<<4))
-#define MAX_SIZE_FRAME 256
+#define MAX_SIZE_FRAME 400
 #define CRC8_POLYNOM  0xD5
 #define MAX_RECEIVE_TRY 3
 
