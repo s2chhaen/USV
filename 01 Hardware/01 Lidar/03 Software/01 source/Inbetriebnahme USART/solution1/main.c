69a0d3e8e7db3d6f03ac7619139abae8329ab69b
hathach23 <thach.hoangha23@gmail.com>
2023-07-24 01:41:44 +0200

    main.c: checksumCrc16() und CRC8() Funktionen und Variablen in init() gelöscht, da sie nicht notwendig sind

--- 01 Hardware/01 Lidar/03 Software/01 source/Inbetriebnahme USART/solution1/main.c
+++ 01 Hardware/01 Lidar/03 Software/01 source/Inbetriebnahme USART/solution1/main.c
@@ -8,52 +8,6 @@
 
 #include "main.h"
 
-
-/**
- * \brief Bilden einer Checksum-Code für ein beliebiges Datenrahmen
- * \param input das zu checkende Array (Datenrahmen)
- * \param length seine Länge
- * \return uint16_t das Checksum-Code für das eingegebene Array
- */
-uint16_t checksumCrc16(uint8_t input[], uint32_t length){
-	const uint16_t genPolynom = 0x8005;
-	uint16_t uCrc16=0;
-	uint8_t temp[]={0,0};
-	for (uint32_t i=0;i<length;i++){
-		temp[1]=temp[0];
-		temp[0]=input[i];
-		if (uCrc16&0x8000){
-			uCrc16 = (uCrc16&0x7fff)<<1;
-			uCrc16 ^= genPolynom;
-			} else{
-			uCrc16<<=1;
-		}
-		uCrc16 ^= (temp[0]|(temp[1]<<8));
-	}
-	return uCrc16;
-}
-
-/*CRC8**********************************************************
-*Funktion bildet eine Checksumme
-*************************************************************/
-uint8_t CRC8( uint8_t *addr, uint8_t len){
-	uint8_t crc = 0;
-	while (len--){
-		uint8_t inbyte = *addr++;
-		uint8_t i;
-		for (i = 8; i; i--){
-			uint8_t mix = ( crc ^ inbyte ) & 0x80;
-			crc <<= 1;
-			if (mix)
-			{
-				crc ^= 0xD5;	// Generatorpolynom 0xD5
-			}
-			inbyte <<= 1;
-		}
-	}
-	return crc;
-}
-
 /**
  * \brief Initalisierung für das ganze Programm
  * \return uint8_t 0:Fehlerfrei, sonst Fehler
@@ -61,8 +15,6 @@ uint8_t CRC8( uint8_t *addr, uint8_t len){
 static uint8_t init(){
 	uint8_t result;
 	uint8_t prescaler = 1;
-	uint16_t rxLength = 900;
-	uint16_t txLength = 900;
 	uint8_t timerResUs = 1;
 	uint16_t timerPre = 1024;
 	uint32_t baudrateSlave = 76800;
@@ -71,7 +23,7 @@ static uint8_t init(){
 	//USART-Beobachter- und Stopuhr-Einheit-Init
 	timerInit(timerResUs,timerPre);
 	//Slave-Device-Init
-	result = initDev(rxLength, txLength, iUSART1, baudrateSlave, USART_CHSIZE_8BIT_gc, USART_PMODE_ODD_gc, USART_SBMODE_1BIT_gc, DISBL_MPC_MODE, DISBL_MPC_MODE, 0, PORTMUX_USARTx_DEFAULT_gc);
+	result = initDev(iUSART1, baudrateSlave, USART_CHSIZE_8BIT_gc, USART_PMODE_ODD_gc, USART_SBMODE_1BIT_gc, DISBL_MPC_MODE, DISBL_MPC_MODE, 0, PORTMUX_USARTx_DEFAULT_gc);
 	sei();//aktiviert das global Interrupt, weil es bisher nicht aktiviert wird
 	return result;
 }
