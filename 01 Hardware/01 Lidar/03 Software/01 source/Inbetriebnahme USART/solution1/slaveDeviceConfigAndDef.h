8e02f993d4c8f22939b2ebc5c0937554562d2dfc
hathach23 <thach.hoangha23@gmail.com>
2023-07-25 23:53:06 +0200

    slaveDeviceConfigAndDef.h : Version 1 von slaveDevice_t Struktur wiederhergestellt

--- 01 Hardware/01 Lidar/03 Software/01 source/Inbetriebnahme USART/solution1/slaveDeviceConfigAndDef.h
+++ 01 Hardware/01 Lidar/03 Software/01 source/Inbetriebnahme USART/solution1/slaveDeviceConfigAndDef.h
@@ -21,27 +21,30 @@
 #define ACTIVE_USART_WATCHER 1
 #define END_SYM 0x0A
 
-//Version 2 von slaveDevice_t
+//Version 1 von slaveDevice_t
 typedef struct{
-	//RX-Buffer
-	volatile uint8_t rx_Buffer[NO_OF_RX_BUFFER][RX_BUFFER_LEN];
-	volatile uint16_t rxByte[NO_OF_RX_BUFFER];
-	volatile uint16_t rx_strReadPtr;
-	const uint16_t rx_LenMax;
-	const uint8_t rx_fifoLenMax;
-	volatile uint8_t rx_readFIFOPtr;
-	volatile uint8_t rx_writeFIFOPtr;
-	//TX-Buffer
-	volatile uint8_t tx_Buffer[TX_BUFFER_LEN];
-	volatile uint16_t toTxByte;
-	volatile uint16_t tx_strReadPtr;
-	const uint16_t tx_LenMax;
-	//Zustand
-	volatile uint8_t uart:2;
-	uint8_t initState:1;
-	uint8_t crcActive:1;
-	volatile uint8_t rxFIFOState:2;//Leer, Belegt, Voll
-	uint8_t nextPhase:1;
+	struct rxUnit{
+		volatile uint8_t rxBuffer[NO_OF_RX_BUFFER][RX_BUFFER_LEN];
+		volatile uint16_t rxByte[NO_OF_RX_BUFFER];
+		volatile uint16_t strReadPtr;
+		const uint16_t rxLenMax;
+		const uint8_t fifoLenMax;
+		volatile uint8_t readFIFOPtr;
+		volatile uint8_t writeFIFOPtr;
+	}rxObj;
+	struct txUnit{
+		volatile uint8_t txBuffer[TX_BUFFER_LEN];
+		volatile uint16_t toTxByte;
+		volatile uint16_t strReadPtr;
+		const uint16_t txLenMax;
+		
+	}txObj;
+	struct status{
+		volatile uint8_t uart:2;
+		uint8_t initState:1;
+		uint8_t crcActive:1;
+		volatile uint8_t rxFIFOState:2;//Leer, Belegt, Voll
+	}statusObj;
 }slaveDevice_t;
 
 typedef enum {
