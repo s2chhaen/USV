b095ce818fe9b35d1bb4d1316265ed8df394160c
hathach23 <thach.hoangha23@gmail.com>
2023-07-31 01:06:51 +0200

    usVMonitorHandlerAPIConfig.h: Kommentar zur Erklärung der Rolle von Macros hinzugefügt

--- 01 Hardware/01 Lidar/03 Software/01 source/Testen mit USVMonitor/Testen mit USVMonitor/usVMonitorHandlerAPIConfig.h
+++ 01 Hardware/01 Lidar/03 Software/01 source/Testen mit USVMonitor/Testen mit USVMonitor/usVMonitorHandlerAPIConfig.h
@@ -4,16 +4,20 @@
  * Created: 7/30/2023 11:05:30 PM
  *  Author: Thach
  * Version: 1.0
- * Revision: 1.0
+ * Revision: 1.1
  */ 
 
 
 #ifndef USVMONITORHANDLERAPICONFIG_H_
 #define USVMONITORHANDLERAPICONFIG_H_
 
+/*Konfigurationsparameter*/
 #define FACTOR_TO_MICROSEC 1000000UL
 #define BAUDRATE_BAUD 250000UL
 #define CHARS_PER_FRAME 11
+
+/*Wenn die Send- und Empfangfunktion keinen Vorgangskontrollmechanismus besitzt, dann muss man die
+* statische Verzögerungsfunktion verwenden, zur Warte auf die Prozesserledigung*/
 #define WAIT_FUNCTION_ACTIVE 0
 
 
