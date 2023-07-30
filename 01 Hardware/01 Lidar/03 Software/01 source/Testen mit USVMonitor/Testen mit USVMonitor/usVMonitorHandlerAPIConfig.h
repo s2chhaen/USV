b83656cff86adfe7053556f977b6d5d793009c01
hathach23 <thach.hoangha23@gmail.com>
2023-07-30 23:12:08 +0200

    usVMonitorHandlerAPIConfig.h: neue Datei erstellt, Zur Definition der Konfiguration für User-Unit-Gerät

---
+++ 01 Hardware/01 Lidar/03 Software/01 source/Testen mit USVMonitor/Testen mit USVMonitor/usVMonitorHandlerAPIConfig.h
@@ -0,0 +1,21 @@
+/*
+ * usVMonitorHandlerAPIConfig.h
+ *
+ * Created: 7/30/2023 11:05:30 PM
+ *  Author: Thach
+ * Version: 1.0
+ * Revision: 1.0
+ */ 
+
+
+#ifndef USVMONITORHANDLERAPICONFIG_H_
+#define USVMONITORHANDLERAPICONFIG_H_
+
+#define FACTOR_TO_MICROSEC 1000000UL
+#define BAUDRATE_BAUD 250000UL
+#define CHARS_PER_FRAME 11
+#define WAIT_FUNCTION_ACTIVE 0
+
+
+
+#endif /* USVMONITORHANDLERAPICONFIG_H_ */
