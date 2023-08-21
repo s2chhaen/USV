58eecc68c999b0de4589a13bf718fdd0377ac229
hathach23 <thach.hoangha23@gmail.com>
2023-08-21 02:38:03 +0200

    quadtreeClass.m: Hinzufüge der Funktion addChildForNode(): vier Unternode für ein Node erzeugt. Attribute node vom Objekt als Cell statt Array festgelegt

--- 01 Hardware/01 Lidar/03 Software/01 source/Lidar-Algorithmus/quadtreeClass.m
+++ 01 Hardware/01 Lidar/03 Software/01 source/Lidar-Algorithmus/quadtreeClass.m
@@ -9,7 +9,6 @@ classdef quadtreeClass < handle
         node=[]
     end
     properties (Access = private)
-        maxChild = 4
         maxPointsPerNode = 4
     end
     properties (GetAccess = public, SetAccess=private)
@@ -47,13 +46,62 @@ classdef quadtreeClass < handle
             end
         end
 
+        % Beim Aufruf wird 4 Kinder für das Node hinzugefügt
+        function cellVal=addChildForNode(obj,node)
+            if nargin~=2
+                error('Eingabe ungültig');
+            elseif isempty(obj) || isempty(node)
+                error('Eingabe ungültig');
+            elseif (obj.id~=3) || (node.id~=2)
+                error('Eingabe ungültig');
+            else
+                checkNode = (node.xValMin < node.xValMax) && ( ...
+                    node.yValMin < node.yValMax);
+                if checkNode
+                    avgX = (node.xValMin + node.xValMax)/2;
+                    avgY = (node.yValMin + node.yValMax)/2;
+                    % Nord-West-Bereich OK
+                    xValMax = avgX;
+                    yValMax = node.yValMax;
+                    xValMin = node.xValMin;
+                    yValMin = avgY;
+                    nordWestNode = quadtreeNodeClass(xValMin,xValMax, ...
+                                                     yValMin,yValMax);
+                    % Nord-Ost-Bereich OK
+                    xValMax = node.xValMax;
+                    yValMax = node.yValMax;
+                    xValMin = avgX;
+                    yValMin = avgY;
+                    nordEastNode = quadtreeNodeClass(xValMin,xValMax, ...
+                                                     yValMin,yValMax);
+                    %Süd-Ost-Bereich OK
+                    xValMax = node.xValMax;
+                    yValMax = avgY;
+                    xValMin = avgX;
+                    yValMin = node.yValMin;
+                    southEastNode = quadtreeNodeClass(xValMin,xValMax, ...
+                                                      yValMin,yValMax);
+                    %Süd-West OK
+                    xValMax = avgX;
+                    yValMax = avgY;
+                    xValMin = node.xValMin;
+                    yValMin = node.yValMin;
+                    southWestNode = quadtreeNodeClass(xValMin,xValMax, ...
+                                                      yValMin,yValMax);
+                    cellVal = {nordWestNode, nordEastNode, southEastNode, ...
+                               southWestNode};
+                else
+                    error('Eingabe ungültig');
+                end
+            end
+        end
     end
 
     methods
         %Constructor
         function obj=quadtreeClass()
             obj.depth = 0;
-            obj.node = [];
+            obj.node = {[]};
             obj.parent = [];
         end
 
@@ -68,7 +116,7 @@ classdef quadtreeClass < handle
                     root = quadtreeNodeClass(bot(1,1),top(1,1), ...
                         bot(1,2),top(1,2));
                     obj.depth = 1;
-                    obj.node = [root];
+                    obj.node = {root};
                     obj.parent = {[0,0]};
                 else
                     error('Eingabe ungültig');
@@ -76,16 +124,6 @@ classdef quadtreeClass < handle
             end
         end
 
-	%TODO Noch in Bearbeitung
-        % function []=addChild(obj,child,parentPosition)
-        %     if (parentPosition(1,1)==0) && (parentPosition(1,2)==0)
-        %         error 'root kann nicht gesetzt mit dieser Methode';
-        %     elseif isempty(obj) ||  isempty(child)
-        %         error 'Eltern oder Kind ist leer';
-        %     elseif (obj.id~=1) ||  (child.id~=1)
-        %         error 'Eltern oder Kind ist kein nodeClass';
-        %     end
-        %     obj.parent = []
-        % end
+
     end
 end
