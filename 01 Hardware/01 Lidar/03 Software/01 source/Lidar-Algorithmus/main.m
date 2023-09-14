041385e0877c66b1378f9b7f92e5b9e6e1a9a6c6
hathach23 <thach.hoangha23@gmail.com>
2023-09-14 19:45:57 +0200

    main.m: Entwurf des Testprogramms

--- 01 Hardware/01 Lidar/03 Software/01 source/Lidar-Algorithmus/main.m
+++ 01 Hardware/01 Lidar/03 Software/01 source/Lidar-Algorithmus/main.m
@@ -23,7 +23,6 @@ end
 
 rSample = sample(:,rCol);
 rSample = transpose(rSample);
-%aSample = arrayfun(@(a) deg2rad(a),sample(:,aCol));
 aSample = sample(:,aCol);
 tVal = transpose(aSample);
 
@@ -34,31 +33,37 @@ rMin = min(rSample);
 order = filterOrder(rMin,rMax);
 fs = filterSampleFreq(sample);
 
-% plot(aSample,rSample,'-o');
-% xlabel('Winkel/Grad');
-% ylabel('Radius/cm');
-
-% polarplot(aSample,rSample,'-o');
-% thetalim([0,180]);
-% rlim([30 40]);
-
 clear lenRA rCol aCol i aSample;
 %IIR - Filter
-fiir = filterChebyshevI_p12;
-[b,a] = sos2tf(fiir.sosMatrix);
+ffir = filterWindowFlatTop_p4;
 result0 = rSample;
-result1 = filter(b,a,rSample);
 
+l = length(ffir.Numerator);
+%Dummy-Sample erzeugen um die Phasenverschiebung zu vermeiden
+rSample = [rSample,ones(1,fix(l/2))*rSample(end)];
 %FIR Filter
-%ffir
-%result1 = filter(Num,1,rSample);
+result1 = filter(ffir.Numerator,1,rSample);
+%Wegwerfen vom Dummy-Sample
+result1 = result1(ceil(l/2):end-1);
 
+figure(1)
+ax(1) = subplot(2,1,1);
 plot(tVal,result0,'-o',tVal,result1,'-o');
 legend('Original Data','Filtered Data');
+grid on
 
 xlabel('Winkel/Grad');
 ylabel('Radius/cm');
-xlim([15,60]);
+
+ax(2) = subplot(2,1,2);
+plot(tVal,20.*log10(result1./result0),'-o');
+legend('magnitude/dB');
+grid on
+
+xlabel('Winkel/Grad');
+ylabel('Radius/cm');
+
+linkaxes(ax,'x')
 
 
 
