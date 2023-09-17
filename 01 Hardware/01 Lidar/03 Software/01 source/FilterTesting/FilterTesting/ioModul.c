dca5d84acda29bd5aa1d410d00a34524eb88002d
hathach23 <thach.hoangha23@gmail.com>
2023-09-17 15:39:40 +0200

    ioModul.c: Funktion getAvaDataLen() implementiert: Berechnung der verfügbaren Datenlänge in Ring-Buffer. Funktion char2Num() implementiert: Umwandlung von Nummerzeichen zur Nummer.  Funktionen max() und min() implementiert: Zur Bestimmung der max. und min. Wert von zwei Nummern. Funktion string2Num() implementiert: Umwandlung einer Nummerzeichenfolge in Nummer. Funktion readFile() refaktorisiert: Ausgabe zum Debuggen gelöscht oder deaktiviert. Funktion getData() implementiert: Zum Lesen der Daten vom Buffer

--- 01 Hardware/01 Lidar/03 Software/01 source/FilterTesting/FilterTesting/ioModul.c
+++ 01 Hardware/01 Lidar/03 Software/01 source/FilterTesting/FilterTesting/ioModul.c
@@ -1,21 +1,53 @@
 #include "ioModul.h"
 
-////Zeichenfolge-Array (Max. Länge der Zeichenfolge = 10)
-//uint8_t dataBuffer[BUFFER_MAX_LENGTH][MAX_LEN_STRING] = {0};
-//uint8_t dataLen[BUFFER_MAX_LENGTH] = {0};
-//uint16_t readIdxPtr = 0;
-//uint16_t writeIdxPtr = 0;
-//uint8_t fullFlag = 0;
-
 static ringBuffer4Str_t buffer = {{{0}},{0},0,0,0};
+
+static uint16_t getAvaDataLen(){
+    uint16_t result = 0;
+    int32_t temp = 0;
+    if(buffer.fullFlag){
+        result = 512;
+    } else{
+        temp = (buffer.writeIdxPtr - buffer.readIdxPtr)%BUFFER_MAX_LENGTH;
+        result = (uint16_t) temp;
+    }
+    return result;
+}
+
+static inline uint8_t char2Num(char input){
+    return input - 48;
+}
+
+static inline int32_t max(int32_t a, int32_t b){
+    return (a>b)?a:b;
+}
+
+static inline int32_t min(int32_t a, int32_t b){
+    return (a<b)?a:b;
+}
+
+static int32_t string2Num(uint8_t* input, uint8_t strLen){
+    int32_t output = 0;
+    //TODO Fehlerfall trennen
+    uint8_t sign = 0;
+    uint8_t beginIdx = 0;
+    uint8_t num = 0;
+    strLen--;//da die letzte Zeichen von String ist '\n' kein Nummer
+    if((input[0]=='-')||(input[0]=='+')){
+        sign = (input[0]=='-')?1:0;
+        beginIdx = 1;
+    }
+    for(unsigned int i = beginIdx; i< strLen; i++){
+        num = char2Num(input[i]);
+        output += (int32_t)num*pow(10,(uint8_t)(strLen-1-i));
+    }
+    output = (sign)?((-1)*output):output;
+    return output;
+}
 
 void readFile(){
     FILE *input = fopen("input.txt","r");
-    //uint8_t checkReading = ferror(input);
-
     if(input!=NULL){
-        uint16_t sizeBuffer = sizeof(buffer.data)/(sizeof(buffer.data[0]));
-        printf("size of string Buffer = %" PRIu32 "\n",sizeBuffer);
         uint8_t stringPtr = 0;
         int32_t character = 0;
         while((character = fgetc(input)) != EOF){
@@ -26,20 +58,15 @@ void readFile(){
                     buffer.dataLen[buffer.writeIdxPtr++] = stringPtr;
                     stringPtr = 0;
                     buffer.fullFlag = (buffer.writeIdxPtr==buffer.readIdxPtr)?1:0;
-                    printf("%c",character);
                 }
             }else{
                 if(stringPtr<MAX_LEN_STRING){
                     buffer.data[buffer.writeIdxPtr][stringPtr++] = character;
-                    putchar(character);
                 } else{
                     printf("Zeichenfolgenlänge überschreitet \n");
                 }
             }
         }
-//        while((letter = fgetc(input)) != EOF){
-//            putchar(letter);
-//        }
     }else{
         printf("File not exist\n");
     }
@@ -47,5 +74,36 @@ void readFile(){
     if(fclose(input)){
         printf("Closing Error");
     }
+
+#ifdef DEBUG_SEC_1
+    //Testen
+    printf("Inhalt von Buffer \n");
+    uint16_t temp = getAvaDataLen();
+    for(unsigned int i = 0;i<temp;i++){
+        printf("Val[%d] = %s \n",i,buffer.data[i]);
+    }
+#endif // DEBUG_SEC_1
 }
+
+void getData(int32_t* data, uint16_t* dataLen){
+    uint8_t checkEmpty = (!buffer.fullFlag) && (buffer.writeIdxPtr==buffer.readIdxPtr);
+    uint16_t avaDataLen = getAvaDataLen();
+    uint16_t temp = 0;
+    if(!checkEmpty){
+        (*dataLen) = (uint16_t)min((*dataLen),avaDataLen);
+        for(int i = 0; i < (*dataLen); i++){
+            temp = buffer.readIdxPtr;
+            buffer.readIdxPtr++;
+            data[i] = string2Num(buffer.data[temp],buffer.dataLen[temp]);
+            if(buffer.readIdxPtr==buffer.writeIdxPtr){
+                buffer.fullFlag = 0;
+                break;
+            }
+        }
+    }
+}
+
+
+
+
 
