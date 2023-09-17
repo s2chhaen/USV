e429ed25281a6f690be8525358e3cb7d561529c8
hathach23 <thach.hoangha23@gmail.com>
2023-09-17 15:55:14 +0200

    Hinzufüge der Koeffizienten der Filter im Form der Header-Daten und exFilter.h zur Verwaltung der Koeffizient

---
+++ 01 Hardware/01 Lidar/03 Software/01 source/FilterTesting/FilterTesting/filterCoefs/extFilter.h
@@ -0,0 +1,9 @@
+#ifndef EXTFILTER_H_INCLUDED
+#define EXTFILTER_H_INCLUDED
+
+#include "firEquirippleCoefs.h"
+#include "firLeastSquaresCoefs.h"
+#include "firMaximallyFlatCoefs.h"
+#include "firWindowKaiserCoefs.h"
+
+#endif // EXTFILTER_H_INCLUDED
