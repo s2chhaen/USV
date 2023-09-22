f307fcf0cf4b763f8f478ad991c07b24d18b042b
hathach23 <thach.hoangha23@gmail.com>
2023-09-22 15:12:28 +0200

    extFilter.h: Inklusion der Header Daten mit Filter Koeffizienten wird gelöscht

--- 01 Hardware/01 Lidar/03 Software/01 source/FilterTesting/FilterTesting/filterCoefs/extFilter.h
+++ 01 Hardware/01 Lidar/03 Software/01 source/FilterTesting/FilterTesting/filterCoefs/extFilter.h
@@ -1,9 +1,5 @@
 #ifndef EXTFILTER_H_INCLUDED
 #define EXTFILTER_H_INCLUDED
 
-#include "firEquirippleCoefs.h"
-#include "firLeastSquaresCoefs.h"
-#include "firMaximallyFlatCoefs.h"
-#include "firWindowKaiserCoefs.h"
 
 #endif // EXTFILTER_H_INCLUDED
