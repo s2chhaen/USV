f2af3716cb824b31a15544a53fa1b29b150dae80
hathach23 <thach.hoangha23@gmail.com>
2023-09-22 17:19:15 +0200

    ioModul.c: Implementation der Funktion readFile() zum Einlesen der Daten von "input_fixed.txt" - Datei

--- 01 Hardware/01 Lidar/03 Software/01 source/FilterTesting/FilterTesting/ioModul.c
+++ 01 Hardware/01 Lidar/03 Software/01 source/FilterTesting/FilterTesting/ioModul.c
@@ -3,6 +3,34 @@
 static ringBuffer4Str_t buffer = {{{0}},{0},0,0,0};
 
 void readFile(){
+    FILE *input = fopen("input_fixed.txt","r");
+    if(input!=NULL){
+        uint8_t stringPtr = 0;
+        int32_t character = 0;
+        while((character = fgetc(input)) != EOF){
+            if(character=='\n'){
+                if(buffer.fullFlag){
+                    printf("Buffer voll \n");
+                } else{
+                    buffer.dataLen[buffer.writeIdxPtr++] = stringPtr;
+                    stringPtr = 0;
+                    buffer.fullFlag = (buffer.writeIdxPtr==buffer.readIdxPtr)?1:0;
+                }
+            }else{
+                if(stringPtr<MAX_LEN_STRING){
+                    buffer.data[buffer.writeIdxPtr][stringPtr++] = character;
+                } else{
+                    printf("Zeichenfolgenlänge überschreitet \n");
+                }
+            }
+        }
+    }else{
+        printf("File not exist\n");
+    }
+
+    if(fclose(input)){
+        printf("Closing Error");
+    }
 }
 
 
