d34980937d3a995368329e9208f2a2c67379a900
hathach23 <thach.hoangha23@gmail.com>
2023-09-22 14:55:23 +0200

    filterFIRScript.m: Skript refraktorisiert: Fälle trennen

--- 01 Hardware/01 Lidar/03 Software/01 source/Lidar-Algorithmus/filterFIRScript.m
+++ 01 Hardware/01 Lidar/03 Software/01 source/Lidar-Algorithmus/filterFIRScript.m
@@ -5,34 +5,55 @@ rMin = min(rSample);
 order = filterOrder(rMin,rMax);
 fs = filterSampleFreq(sample);
 
+draw = 0;
+filtered = 1;
+
 %FIR - Filter
-ffir = filterWindowKaiser_p10;%Eingetragen ist die Filter Parameter
-result0 = rSample;
+if strcmp(choosedFIR,'wk')
+    ffir = filterWindowKaiser_p10;%Eingetragen ist die Filter Parameter
+elseif strcmp(choosedFIR,'lsq')
+    ffir = filterLeastSquares_p11;%Eingetragen ist die Filter Parameter
+elseif strcmp(choosedFIR,'eq')
+    ffir = filterEquiripple_p3;%Eingetragen ist die Filter Parameter
+elseif strcmp(choosedFIR,'wb')
+    ffir = filterWindowBarlett_p2;
+elseif strcmp(choosedFIR,'wr')
+    ffir = filterWindowRectangular_p3;
+elseif strcmp(choosedFIR,'wt')
+    ffir = filterWindowTriangular_p1;
+else
+    filtered = 0;
+    draw = 0;
+end
 
-l = length(ffir.Numerator);
-%Dummy-Sample erzeugen um die Phasenverschiebung zu vermeiden
-rSample = [rSample,ones(1,fix(l/2))*rSample(end)];
-%FIR Filter
-result1 = filter(ffir.Numerator,1,rSample);
-%Wegwerfen vom Dummy-Sample
-result1 = result1(ceil(l/2):end-1);
+if filtered == 1
+    result0 = rSample;
+    tempR = rSample;
+    l = length(ffir.Numerator);
+    %Dummy-Sample erzeugen um die Phasenverschiebung zu vermeiden
+    tempR = [tempR,ones(1,fix(l/2))*tempR(end)];
+    %FIR Filter
+    result1 = filter(ffir.Numerator,1,tempR);
+    %Wegwerfen vom Dummy-Sample
+    result1 = result1(ceil(l/2):end-1);
+end
 
-figure(1)
-ax(1) = subplot(2,1,1);
-plot(tVal,result0,'-o',tVal,result1,'-o');
-legend('Original Data','Filtered Data');
-grid on
+if draw == 1
+    figure(figureNo)
+    figureNo = figureNo + 1;
+    ax(1) = subplot(2,1,1);
+    plot(tVal,result0,'-o',tVal,result1,'-o');
+    legend('Original Data','Filtered Data');
+    grid on
+    xlabel('Winkel/Grad');
+    ylabel('Radius/cm');
+    ax(2) = subplot(2,1,2);
+    plot(tVal,20.*log10(result1./result0),'-o');
+    legend('magnitude/dB');
+    grid on
+    xlabel('Winkel/Grad');
+    ylabel('Radius/cm');
+    linkaxes(ax,'x')
+end
 
-xlabel('Winkel/Grad');
-ylabel('Radius/cm');
-
-ax(2) = subplot(2,1,2);
-plot(tVal,20.*log10(result1./result0),'-o');
-legend('magnitude/dB');
-grid on
-
-xlabel('Winkel/Grad');
-ylabel('Radius/cm');
-
-linkaxes(ax,'x')
-
+clear draw filtered tempR
