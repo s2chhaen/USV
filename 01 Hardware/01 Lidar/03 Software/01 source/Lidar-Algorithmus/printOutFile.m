344d0c6be9d29124d220889ba1f3ca9da32969a1
hathach23 <thach.hoangha23@gmail.com>
2023-09-22 14:51:31 +0200

    printOutFile.m: Skript zum Ausdrucken der Ausgaben in einer txt-Datei

---
+++ 01 Hardware/01 Lidar/03 Software/01 source/Lidar-Algorithmus/printOutFile.m
@@ -0,0 +1,25 @@
+function val = printOutFile(type,filterName,value)
+    val = 0;
+    if strcmp(type,'fixed')
+        path = strcat('../FilterTesting/FilterTesting/output_',filterName,'_fixed.txt');
+        fileID = fopen(path,'w+','l','UTF-8');
+        %Umwandlung zur Fixpoint-Form mit 15 Bits für Nachkommateil
+        %Fixed Point Format 17.15
+        fractionLen = 15;
+        fixedPoint = zeros(1,numel(value));
+        for i=1:numel(value)
+            fixedPoint(i) = floor(value(i)*2^fractionLen);
+        end
+        %Schreiben der Daten in "input_fixed.txt"
+        fprintf(fileID,'%d \n',fixedPoint);
+        %Schließen der Datei
+    elseif strcmp(type,'float')
+        path = strcat('../FilterTesting/FilterTesting/output_',filterName,'_float.txt');
+        fileID = fopen(path,'w+','l','UTF-8');
+        %Schreiben der Daten in "input_fixed.txt"
+        fprintf(fileID,'%7.4f \n',value);
+        %Schließen der Datei
+    end
+    fclose(fileID);
+end
+
