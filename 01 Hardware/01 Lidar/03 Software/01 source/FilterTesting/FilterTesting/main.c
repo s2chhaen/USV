eee7850f6c8efda257314ac2a64dc74a15c5fae0
hathach23 <thach.hoangha23@gmail.com>
2023-09-26 12:53:08 +0200

    main.c: Testlauf in Prozess

--- 01 Hardware/01 Lidar/03 Software/01 source/FilterTesting/FilterTesting/main.c
+++ 01 Hardware/01 Lidar/03 Software/01 source/FilterTesting/FilterTesting/main.c
@@ -1,7 +1,20 @@
 #include "main.h"
 
+#include <stdint.h>
+#include "filter_IIR.h"
+#include "ioModul.h"
+#include "filterCoefs/mf_qFormFilterCof.h"
+
+double a = 1*1.00000;
 
 int main()
 {
+    int32_t temp[BUFFER_MAX_LENGTH]={0};
+    int32_t filtered[BUFFER_MAX_LENGTH]={0};
+    uint16_t len = sizeof(temp)/sizeof(int32_t);
+    readFile();
+    getData(temp,&len);
+    //Test OK
+    iir_init((int16_t*)mf_num,mf_numLen,(int16_t*)mf_den,mf_denLen);
     return 0;
 }
