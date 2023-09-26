591543cecd56980a6a98955a3c26ea8fd3bbd021
hathach23 <thach.hoangha23@gmail.com>
2023-09-26 12:14:29 +0200

    filter_IIR.c: Deklaration der neuen globalen Variablen zum Speichern der alten Berechnungsergebnisse (Floating-Point-Zahl)

--- 01 Hardware/01 Lidar/03 Software/01 source/FilterTesting/FilterTesting/filter_IIR.c
+++ 01 Hardware/01 Lidar/03 Software/01 source/FilterTesting/FilterTesting/filter_IIR.c
@@ -3,11 +3,11 @@
 static uint8_t init = 0;
 static int32_t ffCofs[IIR_FILTER_ORDER+1]={0};
 static int32_t fbCofs[IIR_FILTER_ORDER+1]={0};
-static ffOldBuffer_t old={0};
+static ffOldBuffer_t old = {0};
 
 static double ffCofsFloat[IIR_FILTER_ORDER+1]={0};
 static double fbCofsFloat[IIR_FILTER_ORDER+1]={0};
-
+static ffOldBufferFloat_t oldFLoat = {0};
 
 void iir_init(int16_t* inputFFCofs, uint16_t ffLen, int16_t* inputFBCofs, uint16_t fbLen){
     uint8_t checkFf = ffLen==(IIR_FILTER_ORDER+1);
