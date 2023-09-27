ebd9f1d13a412da899f60f8b68cfb3702f499d80
hathach23 <thach.hoangha23@gmail.com>
2023-09-27 15:00:58 +0200

    filter_FIR.h: Inklusion der notwendigen Bibliotheken

--- 01 Hardware/01 Lidar/03 Software/01 source/FilterTesting/FilterTesting/filter_FIR.h
+++ 01 Hardware/01 Lidar/03 Software/01 source/FilterTesting/FilterTesting/filter_FIR.h
@@ -1,6 +1,13 @@
 #ifndef FILTER_FIR_H_INCLUDED
 #define FILTER_FIR_H_INCLUDED
 
+#include <string.h>
+#include <inttypes.h>
+#include <stdio.h>
+#include <inttypes.h>
+#include <stdint.h>
+#include "filterConfig.h"
+
 
 
 #endif // FILTER_FIR_H_INCLUDED
