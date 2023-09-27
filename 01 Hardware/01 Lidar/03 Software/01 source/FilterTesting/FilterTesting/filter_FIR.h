b7a166eac57cc17f22eaaa752bcd7f7455c0f0ca
hathach23 <thach.hoangha23@gmail.com>
2023-09-27 15:28:47 +0200

    filter_FIR.h: Definition der notwendigen Paramter für Speichern-Ring-FIFO und die Struktur für Ring-FIFO (für Nummer von Q-Format)

--- 01 Hardware/01 Lidar/03 Software/01 source/FilterTesting/FilterTesting/filter_FIR.h
+++ 01 Hardware/01 Lidar/03 Software/01 source/FilterTesting/FilterTesting/filter_FIR.h
@@ -8,6 +8,15 @@
 #include <stdint.h>
 #include "filterConfig.h"
 
+#define FIR_FILTER_ORDER 3U
+#define FIR_OLD_VALUES_BUFFER_LEN_BITS ((FIR_FILTER_ORDER-1U)/2U + 1U)
+#define FIR_OLD_VALUES_BUFFER_LEN (1<<FIR_OLD_VALUES_BUFFER_LEN_BITS)
+
+typedef struct{
+    int32_t data[FIR_OLD_VALUES_BUFFER_LEN];
+    uint8_t beginIdx :FIR_OLD_VALUES_BUFFER_LEN_BITS;
+    uint8_t endIdx   :FIR_OLD_VALUES_BUFFER_LEN_BITS;
+}ffOldBufferFIR_t;
 
 
 #endif // FILTER_FIR_H_INCLUDED
