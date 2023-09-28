b8dffd0550330a93282aa9c61626193674800d5e
hathach23 <thach.hoangha23@gmail.com>
2023-09-28 12:19:30 +0200

    filter_FIR.c: Funktion qFormatARM2TI() von IIR_Filter.c Datei hier kopieren aufgrund der Notwendigkeit

--- 01 Hardware/01 Lidar/03 Software/01 source/FilterTesting/FilterTesting/filter_FIR.c
+++ 01 Hardware/01 Lidar/03 Software/01 source/FilterTesting/FilterTesting/filter_FIR.c
@@ -4,5 +4,15 @@ static uint8_t init = 0;
 static int32_t ffCofs[FIR_FILTER_ORDER+1]={0};
 static ffOldBufferFIR_t old = {0};
 
+static int32_t qFormatARM2TI(int32_t input, uint16_t bits){
+    int32_t result = 0;
+    static const uint16_t bitLenMax = 20;
+    const int32_t cFactorARM = (1<<bits) -1 ;
+    if(bits<bitLenMax){
+        result = (int32_t)((int64_t)input<<bits)/((int64_t)cFactorARM);
+    }
+    return result;
+}
+
 void fir_init(int16_t* inputFFCofs, uint16_t ffLen){
 }
