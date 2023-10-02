6c9e64551293a73a90967a84318d0cdd8a7f66e4
hathach23 <thach.hoangha23@gmail.com>
2023-10-02 15:49:15 +0200

    filterIIR.m: Funktion filterIIR() implementiert, um die filterIIRScript zu ersetzen

---
+++ 01 Hardware/01 Lidar/03 Software/01 source/Lidar-Algorithmus/filterIIR.m
@@ -0,0 +1,28 @@
+function outputVal = filterIIR(inputVal, inputTi, filterObj, draw, phase, figureNo)
+[b,a] = sos2tf(filterObj.sosMatrix);
+
+temp0 = [inputVal ones(1,phase)*inputVal(end)];
+temp0 = filter(b,a,temp0);
+temp0 = temp0((phase+1):end);
+outputVal = temp0;
+
+if draw == 1
+    figure(figureNo);
+
+    ax(1) = subplot(2,1,1);
+    plot(inputTi,inputVal,'-o',inputTi,outputVal,'-o');
+    legend('Original Data','Filtered Data');
+    grid on
+    xlabel('Winkel/Grad');
+    ylabel('Radius/cm');
+
+    ax(2) = subplot(2,1,2);
+    plot(inputTi,20.*log10(outputVal./inputVal),'-o');
+    legend('magnitude/dB');
+    grid on
+    xlabel('Winkel/Grad');
+    ylabel('Radius/dB');
+    linkaxes(ax,'x');
+end
+
+end
