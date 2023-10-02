6c268e7a04fe9049335cca2a2ea82111e9d03ddc
hathach23 <thach.hoangha23@gmail.com>
2023-10-02 16:58:44 +0200

    main.c: Anordnung des Testlaufs für Window-Rectangular-FIR-Filter, funktioniert gut

--- 01 Hardware/01 Lidar/03 Software/01 source/FilterTesting/FilterTesting/main.c
+++ 01 Hardware/01 Lidar/03 Software/01 source/FilterTesting/FilterTesting/main.c
@@ -9,6 +9,7 @@
 #include "filterCoefs/lsq_qFormFilterCof.h"
 #include "filterCoefs/eq_qFormFilterCof.h"
 #include "filterCoefs/wb_qFormFilterCof.h"
+#include "filterCoefs/wr_qFormFilterCof.h"
 
 int main()
 {
@@ -56,12 +57,20 @@ int main()
     writeFile((uint8_t*)"output_eq_fixed_c",sizeof("output_eq_fixed_c"),CSV_FORM,filtered,len);
 #endif // SEG_5_MAIN
 
-#define SEG_6_MAIN
+//#define SEG_6_MAIN
 #ifdef SEG_6_MAIN
     fir_init((int16_t*)wb_num,wb_numLen);
     fir_runFiP(temp,filtered,len);
     writeFile((uint8_t*)"output_wb_fixed_c",sizeof("output_wb_fixed_c"),TEXT_FORM,filtered,len);
     writeFile((uint8_t*)"output_wb_fixed_c",sizeof("output_wb_fixed_c"),CSV_FORM,filtered,len);
 #endif // SEG_6_MAIN
+
+#define SEG_7_MAIN
+#ifdef SEG_7_MAIN
+    fir_init((int16_t*)wr_num,wr_numLen);
+    fir_runFiP(temp,filtered,len);
+    writeFile((uint8_t*)"output_wr_fixed_c",sizeof("output_wr_fixed_c"),TEXT_FORM,filtered,len);
+    writeFile((uint8_t*)"output_wr_fixed_c",sizeof("output_wr_fixed_c"),CSV_FORM,filtered,len);
+#endif // SEG_7_MAIN
     return 0;
 }
