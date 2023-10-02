be7cdd917535a639c28224cdae99413ae91cb4eb
hathach23 <thach.hoangha23@gmail.com>
2023-10-02 16:52:40 +0200

    main.m: Ersetzt des Einsatzes von filterIIRScript und filterFIRScript durch zwei Funktionen filterIIR() und filterFIR(): Falltrennung für die Versuche (die Teste)

--- 01 Hardware/01 Lidar/03 Software/01 source/Lidar-Algorithmus/main.m
+++ 01 Hardware/01 Lidar/03 Software/01 source/Lidar-Algorithmus/main.m
@@ -8,64 +8,89 @@
 clc;
 format shortG
 
-LidarDataExtract; %Ausführung der LidarDataExtract.m Skript
-lenRA = size(dataOutputRA);
-rCol = 1;%Radius
-aCol = 2;%Winkel
-sample = zeros(lenRA(1,1),lenRA(1,2)); %Stichprobe zum Testen
+test1_active = 1;
+test2_active = 1;
+
+if test1_active==1
+    LidarDataExtract; %Ausführung der LidarDataExtract.m Skript
+    lenRA = size(dataOutputRA);
+    rCol = 1;%Radius
+    aCol = 2;%Winkel
+    sample = zeros(lenRA(1,1),lenRA(1,2)); %Stichprobe zum Testen
 
 
-%Durchschnittberechnung
-for i=1:lenRA(1)   
-    sample(i,rCol) = (sum(dataOutputRA(i,rCol,:),'all')/lenRA(1,3));
-    sample(i,aCol) = (sum(dataOutputRA(i,aCol,:),'all')/lenRA(1,3));
+    %Durchschnittberechnung
+    for i=1:lenRA(1)
+        sample(i,rCol) = (sum(dataOutputRA(i,rCol,:),'all')/lenRA(1,3));
+        sample(i,aCol) = (sum(dataOutputRA(i,aCol,:),'all')/lenRA(1,3));
+    end
+
+    rSample = sample(:,rCol);
+    rSample = transpose(rSample);
+    aSample = sample(:,aCol);
+    tVal = transpose(aSample);
+
+    printInFile('fixed',rSample);
+    printInFile('float',rSample);
+
+    %IIR mit Maximally Flat Entwurfsmethode
+    figureNo = 1;
+    draw = 1;
+    phase = 1;
+    filterObj = filterMaximallyFlatFIR_p4;
+    output = filterIIR(rSample, tVal, filterObj, draw, phase, figureNo);
+    figureNo = figureNo + 1;
+    printOutFile('fixed','mf',output);
+    printOutFile('float','mf',output);
+
+    %FIR mit Window-Kaiser Entwurfsmethode
+    phase = 2;
+    filterObj = filterWindowKaiser_p10;
+    filterFIR(rSample, tVal, filterObj, draw, phase, figureNo);
+    figureNo = figureNo + 1;
+    printOutFile('fixed','wk',output);
+    printOutFile('float','wk',output);
+
+    %FIR mit Least-Square Entwurfsmethode
+    phase = 2;
+    filterObj = filterLeastSquares_p11;
+    filterFIR(rSample, tVal, filterObj, draw, phase, figureNo);
+    figureNo = figureNo + 1;
+    printOutFile('fixed','lsq',output);
+    printOutFile('float','lsq',output);
+
+    %FIR mit Equiripple Entwurfsmethode
+    phase = 2;
+    filterObj = filterEquiripple_p3;
+    filterFIR(rSample, tVal, filterObj, draw, phase, figureNo);
+    figureNo = figureNo + 1;
+    printOutFile('fixed','eq',output);
+    printOutFile('float','eq',output);
+
+    %FIR mit Window-Bartlett Entwurfsmethode
+    phase = 2;
+    filterObj = filterWindowBarlett_p2;
+    filterFIR(rSample, tVal, filterObj, draw, phase, figureNo);
+    figureNo = figureNo + 1;
+    printOutFile('fixed','wb',output);
+    printOutFile('float','wb',output);
+
+    %FIR mit Window-Rectangular Entwurfsmethode
+    phase = 2;
+    filterObj = filterWindowRectangular_p3;
+    filterFIR(rSample, tVal, filterObj, draw, phase, figureNo);
+    figureNo = figureNo + 1;
+    printOutFile('fixed','wr',output);
+    printOutFile('float','wr',output);
+
+    %FIR mit Window-Rectangular Entwurfsmethode
+    phase = 2;
+    filterObj = filterWindowTriangular_p1;
+    filterFIR(rSample, tVal, filterObj, draw, phase, figureNo);
+    figureNo = figureNo + 1;
+    printOutFile('fixed','wt',output);
+    printOutFile('float','wt',output);
 end
-
-rSample = sample(:,rCol);
-rSample = transpose(rSample);
-aSample = sample(:,aCol);
-tVal = transpose(aSample);
-
-printInFile('fixed',rSample);
-printInFile('float',rSample);
-
-%IIR
-figureNo = 1;
-choosedIIR = 'mf';
-filterIIRScript;
-printOutFile('fixed',choosedIIR,result1);
-printOutFile('float',choosedIIR,result1);
-
-%FIR
-choosedFIR = 'wk';
-filterFIRScript;
-printOutFile('fixed',choosedFIR,result1);
-printOutFile('float',choosedFIR,result1);
-
-choosedFIR = 'lsq';
-filterFIRScript;
-printOutFile('fixed',choosedFIR,result1);
-printOutFile('float',choosedFIR,result1);
-
-choosedFIR = 'eq';
-filterFIRScript;
-printOutFile('fixed',choosedFIR,result1);
-printOutFile('float',choosedFIR,result1);
-
-choosedFIR = 'wb';
-filterFIRScript;
-printOutFile('fixed',choosedFIR,result1);
-printOutFile('float',choosedFIR,result1);
-
-choosedFIR = 'wr';
-filterFIRScript;
-printOutFile('fixed',choosedFIR,result1);
-printOutFile('float',choosedFIR,result1);
-
-choosedFIR = 'wt';
-filterFIRScript;
-printOutFile('fixed',choosedFIR,result1);
-printOutFile('float',choosedFIR,result1);
 
 
 
