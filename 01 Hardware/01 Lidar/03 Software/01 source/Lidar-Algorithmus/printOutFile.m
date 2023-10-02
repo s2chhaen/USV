0a137ac7a138f4a94bb1af82c1398bbfbe3295f0
hathach23 <thach.hoangha23@gmail.com>
2023-10-02 17:03:09 +0200

    printInFile.m und printOutFile.m: Änderung des Suffix der Ausgabendatei

--- 01 Hardware/01 Lidar/03 Software/01 source/Lidar-Algorithmus/printOutFile.m
+++ 01 Hardware/01 Lidar/03 Software/01 source/Lidar-Algorithmus/printOutFile.m
@@ -1,20 +1,20 @@
 function val = printOutFile(type,filterName,value)
     val = 0;
     if strcmp(type,'fixed')
-        path = strcat('../FilterTesting/FilterTesting/output_',filterName,'_fixed.txt');
+        path = strcat('../FilterTesting/FilterTesting/output_',filterName,'_fixed_matlab.txt');
         fileID = fopen(path,'w+','l','UTF-8');
         %Umwandlung zur Fixpoint-Form mit 15 Bits für Nachkommateil
         %Fixed Point Format 17.15
         fractionLen = 15;
         fixedPoint = zeros(1,numel(value));
         for i=1:numel(value)
-            fixedPoint(i) = floor(value(i)*2^fractionLen);
+            fixedPoint(i) = floor(value(i)*2^fractionLen);%TI-Form
         end
         %Schreiben der Daten in "input_fixed.txt"
         fprintf(fileID,'%d \n',fixedPoint);
         %Schließen der Datei
     elseif strcmp(type,'float')
-        path = strcat('../FilterTesting/FilterTesting/output_',filterName,'_float.txt');
+        path = strcat('../FilterTesting/FilterTesting/output_',filterName,'_float_matlab.txt');
         fileID = fopen(path,'w+','l','UTF-8');
         %Schreiben der Daten in "input_fixed.txt"
         fprintf(fileID,'%7.4f \n',value);
