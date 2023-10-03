9df340ee17e60c5ea92008abaea46f710e2391d1
hathach23 <thach.hoangha23@gmail.com>
2023-10-03 13:36:38 +0200

    main.m: 3. Versuch bei A = 5001 und Ton = 200 Samples

--- 01 Hardware/01 Lidar/03 Software/01 source/Lidar-Algorithmus/main.m
+++ 01 Hardware/01 Lidar/03 Software/01 source/Lidar-Algorithmus/main.m
@@ -103,7 +103,7 @@ if test3_active == 1
     % Original Signal
     % Der Bereich A, deren Werte gleicheinander und ungleich 
     % 0 sind: Wiederholungsmal immer gerade
-    repeatedTimeValMid = 150;
+    repeatedTimeValMid = 100;
     midVal = 5000;
     %Bereich von 0° zum 180° mit Auflösung von 0.5°
     tVal = 0:0.5:180;
@@ -115,43 +115,43 @@ if test3_active == 1
     
     figureNo = 1;
     %Mit dem Maximally-Flat-Methode-entworfenen Filter
-    draw = 0;
+    draw = 1;
     phase = 1;
     filterObj = filterMaximallyFlatFIR_p4;
     filterIIR(xVal, tVal, filterObj, draw, phase, figureNo);
     figureNo = figureNo + 1;
     %Mit dem Window-Kaiser-Methode-entworfenen Filter
-    draw = 0;
+    draw = 1;
     phase = 2;
     filterObj = filterWindowKaiser_p10;
     filterFIR(xVal, tVal, filterObj, draw, phase, figureNo);
     figureNo = figureNo + 1;
     %Mit dem Least-Squares-Methode-entworfenen Filter
-    draw = 0;
+    draw = 1;
     phase = 2;
     filterObj = filterLeastSquares_p11;
     filterFIR(xVal, tVal, filterObj, draw, phase, figureNo);
     figureNo = figureNo + 1;
     %Mit dem Equiripple-Methode-entworfenen Filter
-    draw = 0;
+    draw = 1;
     phase = 2;
     filterObj = filterEquiripple_p3;
     filterFIR(xVal, tVal, filterObj, draw, phase, figureNo);
     figureNo = figureNo + 1;
     %Mit dem Window-Bartlett-Methode-entworfenen Filter
-    draw = 0;
+    draw = 1;
     phase = 2;
     filterObj = filterWindowBarlett_p2;
     filterFIR(xVal, tVal, filterObj, draw, phase, figureNo);
     figureNo = figureNo + 1;
     %Mit dem Window-Rectangular-Methode-entworfenen Filter
-    draw = 0;
+    draw = 1;
     phase = 2;
     filterObj = filterWindowRectangular_p3;
     filterFIR(xVal, tVal, filterObj, draw, phase, figureNo);
     figureNo = figureNo + 1;
     %Mit dem Window-Triangular-Methode-entworfenen Filter
-    draw = 0;
+    draw = 1;
     phase = 2;
     filterObj = filterWindowTriangular_p1;
     filterFIR(xVal, tVal, filterObj, draw, phase, figureNo);
