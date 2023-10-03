6a40bbf992865a0fe0baebcec286435e405eb25e
hathach23 <thach.hoangha23@gmail.com>
2023-10-03 18:28:33 +0200

    main.m: Versuch 2. : Implementation für eq-Filter fertig

--- 01 Hardware/01 Lidar/03 Software/01 source/Lidar-Algorithmus/main.m
+++ 01 Hardware/01 Lidar/03 Software/01 source/Lidar-Algorithmus/main.m
@@ -172,7 +172,7 @@ if test2_active == 1
     slopeF = rms(slopeF);
     diff_wk = slopeO/slopeF;
 
-    %Mit dem Least-Squares-Methode-entworfenen Filter
+    % Mit dem Least-Squares-Methode-entworfenen Filter
     draw = 0;
     phase = 2;
     filterObj = filterLeastSquares_p11;
@@ -195,6 +195,30 @@ if test2_active == 1
     slopeO = rms(slopeO);
     slopeF = rms(slopeF);
     diff_lsq = slopeO/slopeF;
+
+    % Mit dem Equiripple-Methode-entworfenen Filter
+    draw = 0;
+    phase = 2;
+    filterObj = filterEquiripple_p3;
+    output = filterFIR(xnVal, tVal, filterObj, draw, phase, figureNo);
+    if draw == 1
+        figureNo = figureNo + 1;
+    end
+    slopeO = slopeArray(xnVal(onIntervalBegin:onIntervalEnd), ...
+                        tVal(onIntervalBegin:onIntervalEnd));
+    slopeF = slopeArray(output(onIntervalBegin:onIntervalEnd), ...
+                        tVal(onIntervalBegin:onIntervalEnd));
+    draw = 0;
+    if draw == 1
+        figure(figureNo);
+        hVal = [1:numel(slopeF)];
+        plot(hVal,slopeO,'-',hVal,slopeF,'-');
+        legend('Original','gefilterten-Signal','Location','northeastoutside');
+        figureNo = figureNo + 1;
+    end
+    slopeO = rms(slopeO);
+    slopeF = rms(slopeF);
+    diff_eq = slopeO/slopeF;
 end
 
 if test3_active == 1
