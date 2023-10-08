045082d1522feb0ec3add802bd8e94e6ced357ec
hathach23 <thach.hoangha23@gmail.com>
2023-10-08 14:54:27 +0200

    Erstellung des Projektes zum Lesen des Lidarprotokolls

---
+++ 01 Hardware/01 Lidar/03 Software/01 source/LidarTest/LidarTest/main.c
@@ -0,0 +1,18 @@
+/*
+ * LidarTest.c
+ *
+ * Created: 10/8/2023 2:49:32 PM
+ * Author : Thach
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
