90c1785c97d1f535ebccbf55ccd9ba812ca833e5
hathach23 <thach.hoangha23@gmail.com>
2023-10-18 15:50:17 +0200

    main.c: Funktion checkConfig() getestet, funktioniert gut

--- 01 Hardware/01 Lidar/03 Software/01 source/lidarProtocolHandler/main.c
+++ 01 Hardware/01 Lidar/03 Software/01 source/lidarProtocolHandler/main.c
@@ -5,11 +5,19 @@
 #include <math.h>
 #include "ioModul.h"
 #include "protocol.h"
+#include "filter_FIR.h"
+#include "FilterCoefs/wr_qFormFilterCof.h"
 
 int main()
 {
     uint8_t data[1024] = {0};
     uint16_t dataLen = sizeof(data)/sizeof(uint8_t);
-    printf("Hello world!\n");
+#define SEG_0_ACTIVE
+#ifdef SEG_0_ACTIVE
+    readFile("31h-LMS-Status-anfordern.txt");
+    getData(data,&dataLen);
+    checkConfig(data,dataLen);
+    dataLen = sizeof(data)/sizeof(uint8_t);
+#endif // SEG_0_ACTIVE
     return 0;
 }
