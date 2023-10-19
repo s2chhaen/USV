062f529241ca5e263b8416d5ee7fde2ed4f45e69
hathach23 <thach.hoangha23@gmail.com>
2023-10-19 15:05:34 +0200

    lidarIf.c und lidarIf.h: ERstellung einer kleinen Programmschnittstelle für Lidar

---
+++ 01 Hardware/01 Lidar/03 Software/01 source/LidarTest/LidarTest/lidarIf/lidarIf.c
@@ -0,0 +1,8 @@
+/*
+ * lidarIf.c
+ *
+ * Created: 10/19/2023 2:58:35 PM
+ *  Author: Thach
+ */ 
+
+#include "lidarIf.h"
---
+++ 01 Hardware/01 Lidar/03 Software/01 source/LidarTest/LidarTest/lidarIf/lidarIf.h
@@ -0,0 +1,16 @@
+/*
+ * lidarIf.h
+ *
+ * Created: 10/19/2023 2:58:05 PM
+ *  Author: Thach
+ */ 
+
+
+#ifndef LIDARIF_H_
+#define LIDARIF_H_
+
+
+
+
+
+#endif /* LIDARIF_H_ */
