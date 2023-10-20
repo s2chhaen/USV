a2312748bd8e59a02840d7ae3ca50bed8fb73df8
hathach23 <thach.hoangha23@gmail.com>
2023-10-20 11:35:52 +0200

    Umbenennung des Prokjetes "Testen mit USVMonitor" zu "Testen mit USVData"

---
+++ 01 Hardware/01 Lidar/03 Software/01 source/Testen mit USVData/Testen mit USVData/Debug/makedep.mk
@@ -0,0 +1,24 @@
+################################################################################
+# Automatically-generated file. Do not edit or delete the file
+################################################################################
+
+ATMegaXX09\ATMegaXX09Clock.c
+
+ATMegaXX09\FIFO\FIFO.c
+
+ATMegaXX09\FIFO\FIFOa.S
+
+ATMegaXX09\Port\Port.c
+
+ATMegaXX09\USART\USART.c
+
+main.c
+
+Math\checksum.c
+
+timerUnit.c
+
+userUnit.c
+
+usvMonitorHandlerAPI.c
+
