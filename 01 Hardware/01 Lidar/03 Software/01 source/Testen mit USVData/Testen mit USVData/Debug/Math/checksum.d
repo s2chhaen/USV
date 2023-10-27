48812a1c8ef281532b9e2afb8cd039c1ebc58bdd
hathach23 <thach.hoangha23@gmail.com>
2023-10-27 16:13:38 +0200

    Löschen der nicht notwendigen Daten bei Umbenennung des Projektes

--- 01 Hardware/01 Lidar/03 Software/01 source/Testen mit USVData/Testen mit USVData/Debug/Math/checksum.d
+++
@@ -1,9 +0,0 @@
-Math/checksum.d Math/checksum.o: ../Math/checksum.c ../Math/checksum.h \
- c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdint.h \
- c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h
-
-../Math/checksum.h:
-
-c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdint.h:
-
-c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h:
