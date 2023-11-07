23d856233761a78ce9a862d1458e5415dbc78793
hathach23 <thach.hoangha23@gmail.com>
2023-11-07 05:14:05 +0100

    timerUnit.h: die Hardware selbst geschriebene Struktur gelöscht, aufgrund der Verfügbarkeit von der Hersteller-HAL-Bibliothek

--- 01 Hardware/01 Lidar/03 Software/01 source/Testen mit USVData/Testen mit USVData/timerUnit.h
+++ 01 Hardware/01 Lidar/03 Software/01 source/Testen mit USVData/Testen mit USVData/timerUnit.h
@@ -38,60 +38,6 @@ typedef struct{
 	uint8_t state:1;
 }timerStatus_t;
 
-typedef union{
-	uint8_t value;
-	struct{
-		uint8_t ENABLE:1;//Bit 0, low bit
-		uint8_t CLKSEL:3;
-		uint8_t :1;
-		uint8_t :1;
-		uint8_t :1;//Bit 7, high bit
-	}valueBitField;
-}TCA0_CTRLA_t;
-
-typedef union{
-	uint8_t value;
-	struct{
-		uint8_t OVF:1;//Bit 0, low bit
-		uint8_t :1;
-		uint8_t :1;
-		uint8_t :1;
-		uint8_t CMP0:1;
-		uint8_t CMP1:1;
-		uint8_t CMP2:1;
-		uint8_t :1;//Bit 7, high bit
-	}valueBitField;
-}TCA0_INTCTRL_t;
-
-typedef union{
-	uint8_t value;
-	struct{
-		uint8_t OVF:1;//Bit 0,1, low bit
-		uint8_t :1;
-		uint8_t :1;
-		uint8_t :1;
-		uint8_t CMP0:1;
-		uint8_t CMP1:1;
-		uint8_t CMP2:1;
-		uint8_t :1;//Bit 7, high bit
-	}valueBitField;
-}TCA0_INTFLAGS_t;
-
-typedef union{
-	volatile uint16_t value;
-	volatile struct{
-		uint8_t lowByte:8;
-		uint8_t highByte:8;
-	}valueBitField;
-}TCA0_CNT_t;
-
-typedef union{
-	volatile uint16_t value;
-	volatile struct{
-		uint8_t lowByte:8;
-		uint8_t highByte:8;
-	}valueBitField;
-}TCA0_PER_t;
 
 enum prescaler{
 	DIV1 = 0x00,
