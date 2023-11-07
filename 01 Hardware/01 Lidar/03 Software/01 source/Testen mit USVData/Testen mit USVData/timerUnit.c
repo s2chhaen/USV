1902290e40c1999e586624a7576f8db144f3a519
hathach23 <thach.hoangha23@gmail.com>
2023-11-07 05:11:09 +0100

    timerUnit.c: globale Variable verändert

--- 01 Hardware/01 Lidar/03 Software/01 source/Testen mit USVData/Testen mit USVData/timerUnit.c
+++ 01 Hardware/01 Lidar/03 Software/01 source/Testen mit USVData/Testen mit USVData/timerUnit.c
@@ -9,8 +9,8 @@
 
 #include "timerUnit.h"
 
-volatile timerStatus_t timer_status = { .init = 0, .rez = REZ_MS};
-volatile uint16_t timer_counter[REZ_MODE_NO] = {0};
+volatile timerStatus_t timer_status = { .init = 0, .rez = REZ_MS, .state = 0};
+volatile int32_t timer_counter[REZ_MODE_NO] = {0};
 
 uint8_t timerInit(uint8_t rezConfig, uint8_t resolution){
 	uint8_t result = NO_ERROR;
