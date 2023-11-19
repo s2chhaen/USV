112d3d9cb9562028df58d5e7cdf089236c7826a8
hathach23 <thach.hoangha23@gmail.com>
2023-11-19 17:11:18 +0100

    Umbenennung

--- 01 Hardware/01 Lidar/03 Software/01 source/Testen mit USVData/Testen mit USVData/usvDataHandlerConfig.h
+++ 01 Hardware/01 Lidar/03 Software/01 source/Testen mit USVData/Testen mit USVData/usvDataHandlerConfig.h
@@ -1,5 +1,5 @@
 /*
- * usVMonitorHandlerAPIConfig.h
+ * usvDataHandlerConfig.h
  *
  * Created: 7/30/2023 11:05:30 PM
  *  Author: Thach
@@ -8,8 +8,8 @@
  */ 
 
 
-#ifndef USVMONITORHANDLERAPICONFIG_H_
-#define USVMONITORHANDLERAPICONFIG_H_
+#ifndef USVDATAHANDLERCONFIG_H_
+#define USVDATAHANDLERCONFIG_H_
 
 /*Konfigurationsparameter*/
 #define FACTOR_TO_MICROSEC 1000000UL
@@ -17,9 +17,9 @@
 #define CHARS_PER_FRAME 11
 
 /*Wenn die Send- und Empfangfunktion keinen Vorgangskontrollmechanismus besitzt, dann muss man die
-* statische Verzögerungsfunktion verwenden, zur Warte auf die Prozesserledigung*/
+* statische VerzÃ¶gerungsfunktion verwenden, zur Warte auf die Prozesserledigung*/
 #define WAIT_FUNCTION_ACTIVE 0
 
 
 
-#endif /* USVMONITORHANDLERAPICONFIG_H_ */
+#endif /* USVDATAHANDLERCONFIG_H_ */
