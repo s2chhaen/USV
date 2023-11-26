a368234162c75f821b0a9ba70249a44edc0bef45
hathach23 <thach.hoangha23@gmail.com>
2023-11-26 16:08:33 +0100

    lidarHandler.h und lidarHandler.c: Erstellung der Header- und Execute-Daten für Lidar-Handler

---
+++ 01 Hardware/01 Lidar/03 Software/01 source/LidarTest/LidarTest/lidarHandler.c
@@ -0,0 +1,8 @@
+/*
+ * lidarHandler.c
+ *
+ * Created: 11/26/2023 4:06:34 PM
+ *  Author: Thach
+ */ 
+
+#include "lidarHandler.h"
---
+++ 01 Hardware/01 Lidar/03 Software/01 source/LidarTest/LidarTest/lidarHandler.h
@@ -0,0 +1,13 @@
+/*
+ * lidarHandler.h
+ *
+ * Created: 11/26/2023 4:03:53 PM
+ *  Author: Thach
+ */ 
+
+
+#ifndef LIDARHANDLER_H_
+#define LIDARHANDLER_H_
+
+
+#endif /* LIDARHANDLER_H_ */
