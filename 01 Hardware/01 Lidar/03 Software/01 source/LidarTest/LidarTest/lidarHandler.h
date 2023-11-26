9b452d993d8f03bed092497a35df474354829620
hathach23 <thach.hoangha23@gmail.com>
2023-11-26 17:51:27 +0100

    lidarHandler.h: Definition der notwendigen Parameter

--- 01 Hardware/01 Lidar/03 Software/01 source/LidarTest/LidarTest/lidarHandler.h
+++ 01 Hardware/01 Lidar/03 Software/01 source/LidarTest/LidarTest/lidarHandler.h
@@ -16,4 +16,11 @@
 #include "ATMegaXX09/FIFO/FIFO.h"
 #include "Math/checksum.h"
 
+#define LIDAR_PROTOCOL_START_SYM 0x02
+#define LIDAR_PROTOCOL_DEFAULT_ADDR 0x00
+#define LIDAR_PROTOCOL_CANCEL_SYM 0x03
+//rsp: response
+#define LIDAR_PROTOCOL_RESP_CONVERT_COEF 0x80
+#define LIDAR_REQ_PROTOCOL_MAX_LEN 20
+
 #endif /* LIDARHANDLER_H_ */
