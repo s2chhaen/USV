ae55d9518940975fa1e54f1a489f5e28d328d0e2
hathach23 <thach.hoangha23@gmail.com>
2023-11-26 17:49:43 +0100

    checksum.h und checksum.c: Aktualisierung des Checksum-Moduls vom Projekt "Testen mit USVData"

--- 01 Hardware/01 Lidar/03 Software/01 source/LidarTest/LidarTest/Math/checksum.c
+++ 01 Hardware/01 Lidar/03 Software/01 source/LidarTest/LidarTest/Math/checksum.c
@@ -2,27 +2,45 @@
  * checksum.c
  *
  * Created: 7/7/2023 9:13:10 AM
- *  Author: Thach
+ * Author: Thach
  */ 
 
 #include "checksum.h"
 
-uint8_t crc8(uint8_t *data, uint16_t len, uint8_t polynom){
-	uint8_t crc = 0;
-	uint8_t mix;
-	uint8_t inbyte;
-	while (len--){
-		inbyte = *data++;
-		for (uint8_t i = 8; i; i--){
-			mix = ( crc ^ inbyte ) & 0x80;
-			crc <<= 1;
-			if (mix){
-				crc ^= polynom;
+static uint8_t crc8Polynom = 0;
+volatile uint8_t crc8LookupTable[ASCII_MAX_LEN] = {0};
+
+void crc8Init(uint8_t polynom){
+	uint8_t temp;
+	crc8Polynom = polynom;
+	for (volatile int i = ASCII_MAX_LEN; i; i--){
+		temp = ASCII_MAX_LEN - i;
+		for (volatile uint8_t j = 8; j; j--){
+			if (temp&0x80){
+				temp = (temp<<1) ^ crc8Polynom;
+			} else{
+				temp = temp << 1;
 			}
-			inbyte <<= 1;
+			
+		}
+		crc8LookupTable[ASCII_MAX_LEN-i] = temp;
+	}
+}
+
+static inline uint8_t crc8OneByte(uint8_t input){
+	return crc8LookupTable[input];
+}
+
+uint8_t crc8CodeGen(uint8_t *data, uint16_t len){
+	uint8_t retVal = 0;
+	uint8_t temp = 0;
+	if ((data != NULL) && len){
+		for (volatile uint16_t i = 0; i < len; i++){
+			temp = data[i] ^ retVal;
+			retVal = crc8LookupTable[temp];
 		}
 	}
-	return crc;
+	return retVal;
 }
 
 uint16_t crc16(uint8_t* input, uint16_t length, uint16_t polynom){
@@ -34,7 +52,7 @@ uint16_t crc16(uint8_t* input, uint16_t length, uint16_t polynom){
 		if (uCrc16&0x8000){
 			uCrc16 = (uCrc16&0x7fff)<<1;
 			uCrc16 ^= polynom;
-			} else{
+		} else{
 			uCrc16<<=1;
 		}
 		uCrc16 ^= (temp[0]|(temp[1]<<8));
--- 01 Hardware/01 Lidar/03 Software/01 source/LidarTest/LidarTest/Math/checksum.h
+++ 01 Hardware/01 Lidar/03 Software/01 source/LidarTest/LidarTest/Math/checksum.h
@@ -10,8 +10,13 @@
 #define CHECKSUM_H_
 
 #include <stdint.h>
+#include <stddef.h>
 
-extern uint8_t crc8(uint8_t* data, uint16_t len, uint8_t polynom);
+#define ASCII_MAX_LEN_BIT 8
+#define ASCII_MAX_LEN (1<<ASCII_MAX_LEN_BIT)
+
+extern void crc8Init(uint8_t polynom);
+extern uint8_t crc8CodeGen(uint8_t* data, uint16_t len);
 extern uint16_t crc16(uint8_t* data, uint16_t len, uint16_t polynom);
 
 #endif /* CHECKSUM_H_ */
