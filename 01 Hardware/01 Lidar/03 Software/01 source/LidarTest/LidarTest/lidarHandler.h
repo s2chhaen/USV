51448033c9f79c9b408c744d0a29ae346703fffb
hathach23 <thach.hoangha23@gmail.com>
2023-11-26 17:53:08 +0100

    lidarHandler.h: Definition der Zustände vom FSM für das Meldung-Polling

--- 01 Hardware/01 Lidar/03 Software/01 source/LidarTest/LidarTest/lidarHandler.h
+++ 01 Hardware/01 Lidar/03 Software/01 source/LidarTest/LidarTest/lidarHandler.h
@@ -30,4 +30,21 @@ enum lidarProtocolBytesPos{//pos: position
 	LIDAR_DATA_INCL_CMD_BYTE_POS //cmd: command, incl: include
 };
 
+enum lidar_promptFsmState{
+	//Empfangen erste 4 Bytes: STX (1 Byte), Addr (1 Byte), Länge (2 Bytes)
+	LIDAR_PROMPT_FSM_START_STATE,
+	//Warten, bis erst 5 Bytes ankommt und checken, empfangen weiter n Bytes (n in Länge-Bytes)
+	LIDAR_PROMPT_FSM_PROMPT_STATE,
+	//Empfangen weitere 2 Bytes (CRC16-Byte)
+	LIDAR_PROMPT_FSM_DATA_N_STATUS_STATE,
+	//checken mit CRC
+	LIDAR_PROMPT_FSM_CHECK_STATE,
+	//Wenn erfolgreich
+	LIDAR_PROMPT_FSM_SUCCESS_STATE,
+	//Wenn Lidar Fehler hat
+	LIDAR_PROMPT_FSM_ERROR_STATE 
+};
+#define LIDAR_PROMPT_FSM_LAST_STATE LIDAR_PROMPT_FSM_ERROR_STATE
+#define LIDAR_PROMPT_FSM_STATE_NUM (LIDAR_PROMPT_FSM_LAST_STATE+1)
+
 #endif /* LIDARHANDLER_H_ */
