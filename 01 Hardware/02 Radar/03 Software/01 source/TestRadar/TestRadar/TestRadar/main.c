00e37cb529d77d497606ba02588e9b66df79cb7c
hathach23 <thach.hoangha23@gmail.com>
2023-12-04 14:55:07 +0100

    Radar-Software: Verschiebung der alten Daten in einem Order

---
+++ 01 Hardware/02 Radar/03 Software/01 source/TestRadar/TestRadar/TestRadar/main.c
@@ -0,0 +1,18 @@
+/*
+ * TestRadar.c
+ *
+ * Created: 12/4/2023 2:51:26 PM
+ * Author : Dan
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
