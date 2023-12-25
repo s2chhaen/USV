7e27277523192d456178791b3a37ee4809808dc1
hathach23 <thach.hoangha23@gmail.com>
2023-12-25 16:06:52 +0100

    registerModel.h: Inklusion der Standard-Int-Header-Datei. Erstellung des Modells für einen 8-Bit Register

--- 01 Hardware/02 Radar/03 Software/01 source/TestRadar/TestRadar/TestRadar/registerModel.h
+++ 01 Hardware/02 Radar/03 Software/01 source/TestRadar/TestRadar/TestRadar/registerModel.h
@@ -9,7 +9,21 @@
 #ifndef REGISTERMODEL_H_
 #define REGISTERMODEL_H_
 
+#include <stdint.h>
 
+typedef union{
+	uint8_t val;
+	struct{
+		uint8_t bit0:1;
+		uint8_t bit1:1;
+		uint8_t bit2:1;
+		uint8_t bit3:1;
+		uint8_t bit4:1;
+		uint8_t bit5:1;
+		uint8_t bit6:1;
+		uint8_t bit7:1;
+	}valBf;
+}reg8Model_t;
 
 
 
