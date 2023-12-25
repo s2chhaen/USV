7105a8a4c88c6d02e304d3e7aa4ed80cb8a55842
hathach23 <thach.hoangha23@gmail.com>
2023-12-25 16:01:14 +0100

    registerModel.h: Hinzufüge der Header-Datei für einen Registermodel

---
+++ 01 Hardware/02 Radar/03 Software/01 source/TestRadar/TestRadar/TestRadar/registerModel.h
@@ -0,0 +1,16 @@
+/*
+ * registerModel.h
+ *
+ * Created: 12/15/2023 2:16:11 AM
+ *  Author: Thach
+ */ 
+
+
+#ifndef REGISTERMODEL_H_
+#define REGISTERMODEL_H_
+
+
+
+
+
+#endif /* REGISTERMODEL_H_ */
