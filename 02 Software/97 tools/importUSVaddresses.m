5337bec9295552f497e5137898bd7e538442dfbb
Joe G <grabow@amesys.de>
2024-01-18 18:02:42 +0100

    test

--- 02 Software/97 tools/importUSVaddresses.m
+++ 02 Software/97 tools/importUSVaddresses.m
@@ -1,12 +1,12 @@
 function usvAddress = importUSVaddresses()
 
-	% Pfad zur JSON-Datei
+	% Pfad zur JSON-Datei einstellen
 	jsonWebPath = 'https://raw.githubusercontent.com/Joe-Grabow/USV/main/02%20Software/97%20tools/datablock.json';
 	
 	% JSON-Datei einlesen
 	data = webread(jsonWebPath);
 	
-	% JSON-Daten decodieren
+	% JSON-Daten decodieren 
 	usvAddress = jsondecode(data);
 	
 	if nargout == 0
