7066e25b83f43ea2e9011e87f5ea7e0e58f0c235
StefanATK <stefan.franke@eah-jena.de>
2024-01-18 18:03:37 +0100

    test

--- 02 Software/97 tools/importUSVaddresses.m
+++ 02 Software/97 tools/importUSVaddresses.m
@@ -1,6 +1,6 @@
 function usvAddress = importUSVaddresses()
 
-	% Pfad zur JSON-Datei
+	% Pfad zur JSON-Datei 
 	jsonWebPath = 'https://raw.githubusercontent.com/Joe-Grabow/USV/main/02%20Software/97%20tools/datablock.json';
 	
 	% JSON-Datei einlesen
