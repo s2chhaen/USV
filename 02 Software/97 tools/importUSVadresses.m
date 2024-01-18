f084e4c9460fc2cb026093cd6c191c6b1154a4f5
StefanATK <stefan.franke@eah-jena.de>
2024-01-18 15:03:35 +0100

    Rename File import.m because of Matlab Warning: "Function import has the same name as a MATLAB builtin. We suggest you rename the function to avoid a potential name conflict."

---
+++ 02 Software/97 tools/importUSVadresses.m
@@ -0,0 +1,16 @@
+function usvAddress = importUSVadresses()
+
+	% Pfad zur JSON-Datei
+	jsonWebPath = 'https://raw.githubusercontent.com/Joe-Grabow/USV/main/02%20Software/97%20tools/datablock.json';
+	
+	% JSON-Datei einlesen
+	data = webread(jsonWebPath);
+	
+	% JSON-Daten decodieren
+	usvAddress = jsondecode(data);
+	
+	if nargout == 0
+		% Zugriff auf die Daten in der Struktur
+		disp(usvAddress);
+	end
+end
