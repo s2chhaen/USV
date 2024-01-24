d1438bf7c949e1275922ee6d78d8532e869b0da8
hathach23 <thach.hoangha23@gmail.com>
2024-01-24 20:39:26 +0100

    lidarHandlerProtocol.h: Definition der Protokoll-bezogenen Parameter

--- 01 Hardware/01 Lidar/03 Software/01 source/LidarMain/LidarMain/LidarMain/lidarHandlerProtocol.h
+++ 01 Hardware/01 Lidar/03 Software/01 source/LidarMain/LidarMain/LidarMain/lidarHandlerProtocol.h
@@ -13,5 +13,15 @@
 
 #include "asciiTable.h"
 
+//Ver: Version, RESO: resolution, BM: bitmap
+#define LIDAR_TYPE_STR "LMS200"
+#define LIDAR_TYPE_STR_LEN (sizeof(LIDAR_TYPE_STR)/sizeof(uint8_t) - 1)
+#define LIDAR_VER_STR "V01.10"
+#define LIDAR_VER_STR_LEN (sizeof(LIDAR_VER_STR)/sizeof(uint8_t) - 1)
+#define LIDAR_DATA_FRAME_CM_CONGIG (0x00 << 14)
+#define LIDAR_DATA_FRAME_SCAN_TYPE_CONFIG (0x00 << 11)
+#define LIDAR_SET_CONFIG (LIDAR_DATA_FRAME_CM_CONGIG|LIDAR_DATA_FRAME_SCAN_TYPE_CONFIG)
+#define LIDAR_SET_CONFIG_BM ((0x03<<14)|(0x07<<11))
+
 
 #endif /* LIDARHANDLERPROTOCOL_H_ */
