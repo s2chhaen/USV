849b0a64b0632d9d55582a5ee10db802a5561c7d
hathach23 <thach.hoangha23@gmail.com>
2024-01-24 16:03:14 +0100

    Hinzufüge der lidarHandler.h Header-Datei

---
+++ 01 Hardware/01 Lidar/03 Software/01 source/LidarMain/LidarMain/LidarMain/lidarHandler.h
@@ -0,0 +1,16 @@
+/*
+ * lidarHandler.h
+ *
+ * Created: 11/26/2023 4:03:53 PM
+ * Author: Thach
+ * Version: 1.0
+ * Revision: 1.1
+ */ 
+
+
+#ifndef LIDARHANDLER_H_
+#define LIDARHANDLER_H_
+
+
+
+#endif /* LIDARHANDLER_H_ */
