0362cecca540952970fafe67eb6e8c42a3ead6b1
hathach23 <thach.hoangha23@gmail.com>
2024-01-24 15:56:09 +0100

    comConfig.h: Hinzufüge der Header-Datei

---
+++ 01 Hardware/01 Lidar/03 Software/01 source/LidarMain/LidarMain/LidarMain/comConfig.h
@@ -0,0 +1,14 @@
+/*
+ * comConfig.h
+ *
+ * Created: 12/15/2023 10:00:07 AM
+ * Author: Thach
+ * Version: 1.0
+ * Revision: 1.0
+ */ 
+
+
+#ifndef COMCONFIG_H_
+#define COMCONFIG_H_
+
+#endif /* COMCONFIG_H_ */
