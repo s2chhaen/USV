5a9fc2cd90cc64aca2494b3f943263a35e092d25
hathach23 <thach.hoangha23@gmail.com>
2024-01-24 15:58:30 +0100

    comConfig.h: Inklusion der notwendigen Bibliothek, Definition der Struktur zum Speichern der USART-Konfigurationsdaten

--- 01 Hardware/01 Lidar/03 Software/01 source/LidarMain/LidarMain/LidarMain/comConfig.h
+++ 01 Hardware/01 Lidar/03 Software/01 source/LidarMain/LidarMain/LidarMain/comConfig.h
@@ -11,4 +11,20 @@
 #ifndef COMCONFIG_H_
 #define COMCONFIG_H_
 
+#include <stdint.h>
+
+
+/*Struktur für Konfiguration der USART-Einheit*/
+typedef struct{
+	uint8_t usartNo:2;
+	uint32_t baudrate;
+	uint8_t usartChSize;
+	uint8_t parity;
+	uint8_t stopbit;
+	uint8_t sync:1;
+	uint8_t mpcm:1;
+	uint8_t address;
+	uint8_t portMux:2;
+}usartConfig_t;
+
 #endif /* COMCONFIG_H_ */
