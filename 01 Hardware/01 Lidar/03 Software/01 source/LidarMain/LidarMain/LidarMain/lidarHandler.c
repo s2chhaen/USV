5207c535a8ac612c451dd3720f5bf307ef5274d6
hathach23 <thach.hoangha23@gmail.com>
2024-01-30 16:07:54 +0100

    lidarHandler.c: Hinzufüge der internen Variablen für dieses Modul

--- 01 Hardware/01 Lidar/03 Software/01 source/LidarMain/LidarMain/LidarMain/lidarHandler.c
+++ 01 Hardware/01 Lidar/03 Software/01 source/LidarMain/LidarMain/LidarMain/lidarHandler.c
@@ -11,3 +11,29 @@
 
 /*Konstante Variable von USART-FIFO*/
 static const uint16_t usartFIFOMaxLen = _FIFO_max_def - 2;
+/*Lokalvariable für Lidar*/
+static uint8_t lidar_addr = LIDAR_PROTOCOL_DEFAULT_ADDR;
+volatile uint16_t lidar_tempChecksumVal = 0;
+//Lokalvariable für Protokoll
+volatile uint8_t lidar_protocol[LIDAR_REQ_PROTOCOL_MAX_LEN] = {0};
+volatile uint8_t lidar_protocolToHandleBytes = 0;
+volatile uint8_t lidar_protocolIdx = 0; //Idx: Index
+//Lokalvariable für RX-Buffer (Ausgabe)
+volatile uint8_t* lidar_rxBuffer;
+volatile uint16_t* lidar_rxBufferStrLen;
+volatile uint16_t lidar_rxBufferToHandleBytes = 0;
+volatile uint16_t lidar_rxBufferIdx = 0;
+volatile uint8_t lidar_expectedRxBytes = 0;
+//Lokalvariable für TX-Buffer
+static uint8_t lidar_txDataBuffer[10];
+//Lokalvariable zur Modul-Verwaltung
+volatile lidarMgr_t lidar_mgr = {0};
+volatile lidarStatus_t  lidar_status = {0};
+volatile usartConfig_t lidar_comParam = {0};
+static int8_t lidar_tryTime = 1; 
+volatile uint16_t lidar_checksumPolynom = 0;
+
+volatile reg8Model_t* lidar_ioStream = NULL;
+volatile uint8_t lidar_programPos = COM_PROGRAMM_NORMAL_POS; 
+
+/* temp-Var zum Speichern des Params von der Rückruffunktion */
