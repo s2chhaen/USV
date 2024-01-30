7d89c5efca8b704eddbf8eebe03e1bf7e473cb66
hathach23 <thach.hoangha23@gmail.com>
2024-01-30 17:09:11 +0100

    lidarDataPrc.c: Deklaration der intern verwendeten Variablen

--- 01 Hardware/01 Lidar/03 Software/01 source/LidarMain/LidarMain/LidarMain/lidarDataPrc.c
+++ 01 Hardware/01 Lidar/03 Software/01 source/LidarMain/LidarMain/LidarMain/lidarDataPrc.c
@@ -9,3 +9,13 @@
 
 #include "lidarDataPrc.h"
 
+/*Lokalvariablen zum Speichern der Filter-Koeffizienten*/
+volatile int32_t fil_coffs[FIL_ORDER+1] = {0};
+/*Lokalvariablen zum Speichern der vorherigen Daten der Eingabe*/
+volatile filOldValBuffer fil_oldVal = {0};
+/*Lokalvariablen zum Speichern des Filter-Zustand*/
+volatile filStatus_t fil_mgr = {0};
+/*Lokalvariablen zum Speichern der zu filternden Daten*/
+volatile int32_t fil_dataBuffer[DATA_BUFFER_LEN] = {0};
+volatile uint16_t fil_dataBufferIdx = 0;
+static uint16_t fil_dataBufferLen = 0;
