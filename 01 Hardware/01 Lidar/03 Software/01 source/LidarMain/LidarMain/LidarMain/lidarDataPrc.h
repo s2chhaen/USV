1186ad8711f86c792f4cfebbcb8dffc7455d86c9
hathach23 <thach.hoangha23@gmail.com>
2024-01-30 16:52:06 +0100

    lidarDataPrc.h: Definition der Parameter für die internen Variablen

--- 01 Hardware/01 Lidar/03 Software/01 source/LidarMain/LidarMain/LidarMain/lidarDataPrc.h
+++ 01 Hardware/01 Lidar/03 Software/01 source/LidarMain/LidarMain/LidarMain/lidarDataPrc.h
@@ -18,5 +18,12 @@
 #include "comConfig.h"
 #include "registerModel.h"
 
+/*Lokale Variable Param*/
+#define FIL_ORDER 3U
+#define FIL_OLD_VALUES_BUFFER_LEN_BITS ((FIL_ORDER-1U)/2U + 1U)
+#define FIL_OLD_VALUES_BUFFER_LEN (1<<FIL_OLD_VALUES_BUFFER_LEN_BITS)
+#define PHASE_SHIFT_SPL_MAX (FIL_ORDER/2 + 1)
+#define DATA_SPL_NUM 361
+#define DATA_BUFFER_LEN (DATA_SPL_NUM+PHASE_SHIFT_SPL_MAX)
 
 #endif /* LIDARFILTER_H_ */
