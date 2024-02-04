9795d605d7ffc9bb88b4a1e5fb38327c362459ec
hathach23 <thach.hoangha23@gmail.com>
2024-02-04 17:34:42 +0100

    main.c: Deklaration der Variablen zur Behandlung des Lidar-Handler-Moduls

--- 01 Hardware/01 Lidar/03 Software/01 source/LidarMain/LidarMain/LidarMain/main.c
+++ 01 Hardware/01 Lidar/03 Software/01 source/LidarMain/LidarMain/LidarMain/main.c
@@ -15,5 +15,11 @@ int main(void){
 	/**** Deklaration der Variablen ****/
 	/* io-stream für Lidar-Handler, Data-Processing, USVData-Handler*/
 	volatile reg8Model_t mainStream = {0};
+	/* LidarHandler-Variablen */
+	volatile uint8_t lidarOutput[LIDAR_RX_BUFFER_MAX_LEN] = {0};
+	volatile uint16_t lidarOutputLen = sizeof(lidarOutput)/sizeof(uint8_t);
+	volatile uint8_t lidarFSMState = LIDAR_MAIN_SYNC_STATE;
+	volatile uint8_t lidarFlagLen = LIDAR_STATUS_REG_LEN_BYTE;
+	const lidarStatus_t* lidarFlag_p = lidar_getStatus();
 }
 
