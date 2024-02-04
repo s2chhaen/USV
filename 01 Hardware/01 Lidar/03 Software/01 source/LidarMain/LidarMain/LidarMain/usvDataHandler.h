983512289798fd80d37bc1215e61ffd815560d96
hathach23 <thach.hoangha23@gmail.com>
2024-02-04 15:36:40 +0100

    usvDataHandler.h: Hinzufüge der Header-Datei

---
+++ 01 Hardware/01 Lidar/03 Software/01 source/LidarMain/LidarMain/LidarMain/usvDataHandler.h
@@ -0,0 +1,15 @@
+/*
+ * usvDataHandler.h
+ *
+ * Created: 7/7/2023 5:25:53 AM
+ * Author: Thach
+ * Version: 1.4
+ * Revision: 1.1
+ */ 
+
+#ifndef USVDATAHANDLER_H_
+#define USVDATAHANDLER_H_
+
+
+
+#endif /* USVDATAHANDLER_H_ */
