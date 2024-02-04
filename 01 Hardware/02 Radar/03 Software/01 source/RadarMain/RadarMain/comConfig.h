275fef29e5d9247067404fac1e4e2022a9fb4e89
hathach23 <thach.hoangha23@gmail.com>
2024-02-04 18:22:34 +0100

    comConfig.h: Definition der für Konfiguration vom USART notwendigen Struktur

--- 01 Hardware/02 Radar/03 Software/01 source/RadarMain/RadarMain/comConfig.h
+++ 01 Hardware/02 Radar/03 Software/01 source/RadarMain/RadarMain/comConfig.h
@@ -17,6 +17,19 @@
 #define STREAM_RADAR_STATUS_BIT_POS 0
 #define STREAM_RADAR_DATA_BIT_POS 1
 
+/*Struktur für Konfiguration der USART-Einheit*/
+typedef struct{
+	uint8_t usartNo:2;
+	uint32_t baudrate;
+	uint8_t usartChSize;
+	uint8_t parity;
+	uint8_t stopbit;
+	uint8_t sync:1;
+	uint8_t mpcm:1;
+	uint8_t address;
+	uint8_t portMux:2;
+}usartConfig_t;
+
 
 
 #endif /* COMCONFIG_H_ */
