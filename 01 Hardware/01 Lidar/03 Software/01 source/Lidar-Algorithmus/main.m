0cd05ff58f798fddeeaee6e688f130941e4d68a3
hathach23 <thach.hoangha23@gmail.com>
2024-02-05 19:41:22 +0100

    main.m: Berechnung der Abtastfrequenz unter Anwendung der Funktion filterSampleFreq()

--- 01 Hardware/01 Lidar/03 Software/01 source/Lidar-Algorithmus/main.m
+++ 01 Hardware/01 Lidar/03 Software/01 source/Lidar-Algorithmus/main.m
@@ -8,26 +8,28 @@ clear;
 clc;
 format shortG
 
+% Setzen dieses Flags zur Aktivierung des Versuch-Skriptes
 % 0. Versuch: Überblick über die ausgewählten Filter-Entwurf-Methode
-test0_active = 0;
+test0_active = 0; % Nicht notwendig
 % 1. Versuch: Zur Untersuchung des Effektes von Filtern 
-test1_active = 0;
+test1_active = 1;
 % 2. Versuch: Zur Bestimmung der Verlustpunkte am Rand
-test2_active = 0;
+test2_active = 1;
 % 3. Versuch: Zur Untersuchung des Effektes von Filtern im Bereich mit der 
 % schwächen Wirkung der Störgrößen
-test3_active = 0;
+test3_active = 1;
 % 5. Versuche: Anwendung des ausgewählten Filters
-test5_active = 0;
+test5_active = 1;
 
+%Berechnung der für das Filter-Design notwendigen Parameter
 lidarMinRange = 6.25;
 lidarMaxRange = 8000;
 lidarMaxAngleRange = 180;
 lidarResoluation = 0.5;
-wf = 1;
+
 
 order = filterOrder(lidarMinRange,lidarMaxRange);
-fs = wf*lidarMaxAngleRange/lidarResoluation;
+fs = filterSampleFreq(lidarMaxAngleRange,lidarResoluation);
 
 if test0_active == 1
     test0
