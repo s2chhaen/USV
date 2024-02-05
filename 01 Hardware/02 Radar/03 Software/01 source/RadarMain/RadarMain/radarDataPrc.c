b977897a8d1d3f32c9551728e6636ffca55ea53e
hathach23 <thach.hoangha23@gmail.com>
2024-02-05 15:15:22 +0100

    radarDataPrc.c: Hinzufüge der Implementation der Funktion data_compareNReturn()

--- 01 Hardware/02 Radar/03 Software/01 source/RadarMain/RadarMain/radarDataPrc.c
+++ 01 Hardware/02 Radar/03 Software/01 source/RadarMain/RadarMain/radarDataPrc.c
@@ -9,3 +9,12 @@
 
 #include "radarDataPrc.h"
 
+extern void data_compareNReturn(float vel, float dis, uint8_t* output_p, uint8_t outputLen){
+	if (outputLen >= 3){
+		int8_t tempVel = (int8_t)(vel * (1 << VEL_FIXED_POINT_BIT));
+		uint16_t tempDis = (uint16_t) (dis *(1 << DIS_FIXED_POINT_BIT));
+		output_p[0] = tempDis & 0xff;
+		output_p[1] = tempDis << 8;
+		memcpy((void*)&output_p[2],(void*)&tempVel,1);
+	}
+}
