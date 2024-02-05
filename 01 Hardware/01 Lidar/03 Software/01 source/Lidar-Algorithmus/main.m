2822ea75ac58a3adca2c65f30364d717406c7568
hathach23 <thach.hoangha23@gmail.com>
2024-02-05 18:58:53 +0100

    main.m: Korrektur der Kommentare und Anpassung der Variablennamen

--- 01 Hardware/01 Lidar/03 Software/01 source/Lidar-Algorithmus/main.m
+++ 01 Hardware/01 Lidar/03 Software/01 source/Lidar-Algorithmus/main.m
@@ -8,17 +8,21 @@ clear;
 clc;
 format shortG
 
-% 0. Versuch: zum Erreichen des Überblick und groben Auswahl der Filter-Methode
+% 0. Versuch: Überblick über die ausgewählten Filter-Entwurf-Methode
 test0_active = 1;
-% 2. Versuch: Zur Untersuchung des Effektes von Filtern 
+% 1. Versuch: Zur Untersuchung des Effektes von Filtern 
+test1_active = 0;
+% 2. Versuch: Zur Bestimmung der Verlustpunkte am Rand
 test2_active = 0;
-% 3. Versuch: Zur Untersuchung des Effektes von Filtern im flachen Bereich
-test3_active = 0;
-% 4. Versuch: Zur Untersuchung des Effektes von Filtern im Bereich mit der 
+% 3. Versuch: Zur Untersuchung des Effektes von Filtern im Bereich mit der 
 % schwächen Wirkung der Störgrößen
-test4_active = 0;
-% checken das Program von C
-test5_active = 1;
+test3_active = 0;
+% 5. Versuche: Anwendung des ausgewählten Filters
+test5_active = 0;
+
+if test0_active == 1
+    test0
+end
 
 if test1_active == 1
     test1
@@ -30,10 +34,6 @@ end
 
 if test3_active == 1
     test3
-end
-
-if test4_active == 1
-    test4
 end
 
 if test5_active == 1
