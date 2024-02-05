d5879314ed75b9dff7423f61fd32b503506000ce
hathach23 <thach.hoangha23@gmail.com>
2024-02-05 18:02:15 +0100

    test3.m: Zusammenpacken der draw Variable an nur eine Stelle

--- 01 Hardware/01 Lidar/03 Software/01 source/Lidar-Algorithmus/test3.m
+++ 01 Hardware/01 Lidar/03 Software/01 source/Lidar-Algorithmus/test3.m
@@ -20,32 +20,26 @@ draw = 1;
 phase = 1;
 filterObj = filterMaximallyFlatFIR_p4;
 filterIIR(xVal, tVal, filterObj, draw, phase, figureNo);
-draw = 1;
 % vom Window-Kaiser-Methode-entworfenen Filter
 phase = 2;
 filterObj = filterWindowKaiser_p10;
 filterFIR(xVal, tVal, filterObj, draw, phase, figureNo);
-draw = 1;
 % vom Least-Squares-Methode-entworfenen Filter
 phase = 2;
 filterObj = filterLeastSquares_p11;
 filterFIR(xVal, tVal, filterObj, draw, phase, figureNo);
-draw = 1;
 % vom Equiripple-Methode-entworfenen Filter
 phase = 2;
 filterObj = filterEquiripple_p3;
 filterFIR(xVal, tVal, filterObj, draw, phase, figureNo);
-draw = 1;
 % vom Window-Bartlett-Methode-entworfenen Filter
 phase = 2;
 filterObj = filterWindowBarlett_p2;
 filterFIR(xVal, tVal, filterObj, draw, phase, figureNo);
-draw = 1;
 % vom Window-Rectangular-Methode-entworfenen Filter
 phase = 2;
 filterObj = filterWindowRectangular_p3;
 filterFIR(xVal, tVal, filterObj, draw, phase, figureNo);
-draw = 1;
 % vom Window-Triangular-Methode-entworfenen Filter
 phase = 2;
 filterObj = filterWindowTriangular_p1;
