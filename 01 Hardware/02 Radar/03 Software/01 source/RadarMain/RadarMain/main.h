d0e4dd413621b2747c3bf65d2081d5f0a29d3247
hathach23 <thach.hoangha23@gmail.com>
2024-02-05 15:20:46 +0100

    main.h: Inklusion der Header-Daten von Timer-Einheit, Radar-Handler-, USV-Data-Handler- und Datenverarbeitungseinheit

--- 01 Hardware/02 Radar/03 Software/01 source/RadarMain/RadarMain/main.h
+++ 01 Hardware/02 Radar/03 Software/01 source/RadarMain/RadarMain/main.h
@@ -15,6 +15,11 @@
 #include <avr/interrupt.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include "radarHandler.h"
+#include "registerModel.h"
+#include "usvDataHandler.h"
+#include "radarDataPrc.h"
+#include "comConfig.h"
 
 
 #endif /* MAIN_H_ */
