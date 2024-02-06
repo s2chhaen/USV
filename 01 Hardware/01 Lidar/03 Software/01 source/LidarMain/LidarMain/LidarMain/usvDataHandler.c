170c547b63219ed0013bb7b8ad4c91431400f328
hathach23 <thach.hoangha23@gmail.com>
2024-02-06 10:46:03 +0100

    usvDataHandler.c: Funktion checkRxData() dokumentiert

--- 01 Hardware/01 Lidar/03 Software/01 source/LidarMain/LidarMain/LidarMain/usvDataHandler.c
+++ 01 Hardware/01 Lidar/03 Software/01 source/LidarMain/LidarMain/LidarMain/usvDataHandler.c
@@ -175,6 +175,16 @@ static inline int16_t getRegLen(uint16_t reg){
 	return result;
 }
 
+/**
+ * \brief Überprüfen der Daten mit Hilfer der CRC8-Checksum-Funktion
+ * 
+ * \param data
+ * \param dataLen
+ * \param rxChecksumValue
+ * \param crc8Polynom
+ * 
+ * \return bool
+ */
 static inline bool checkRxData(uint8_t* data,uint8_t dataLen, uint8_t rxChecksumValue, uint8_t crc8Polynom){
 	return crc8CodeGen(data,(uint16_t)dataLen)==rxChecksumValue;
 }
