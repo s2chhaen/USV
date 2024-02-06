20a9b00e5b3b684c9db6f265dc16cc6ef9c28c2e
hathach23 <thach.hoangha23@gmail.com>
2024-02-06 10:23:00 +0100

    usvDataHandler.c: Beschreibung hinzugefügt

--- 01 Hardware/01 Lidar/03 Software/01 source/LidarMain/LidarMain/LidarMain/usvDataHandler.c
+++ 01 Hardware/01 Lidar/03 Software/01 source/LidarMain/LidarMain/LidarMain/usvDataHandler.c
@@ -1,10 +1,10 @@
 /*
  * usvDataHandler.c
- *
+ * Description: Quellcode-Datei der usvDataHandler.h
  * Created: 7/7/2023 8:42:59 AM
  * Author: Thach
  * Version: 1.4
- * Revision: 1.2
+ * Revision: 1.3
  */
 
 #include "usvDataHandler.h"
