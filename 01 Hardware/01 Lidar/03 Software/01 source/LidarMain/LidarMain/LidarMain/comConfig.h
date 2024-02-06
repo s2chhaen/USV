072e62d1a488d7a4f14e97ad1bb97630f4be89c0
hathach23 <thach.hoangha23@gmail.com>
2024-02-06 09:31:46 +0100

    comConfig: Hinzufüge der Beschreibung

--- 01 Hardware/01 Lidar/03 Software/01 source/LidarMain/LidarMain/LidarMain/comConfig.h
+++ 01 Hardware/01 Lidar/03 Software/01 source/LidarMain/LidarMain/LidarMain/comConfig.h
@@ -1,10 +1,10 @@
 /*
  * comConfig.h
- *
+ * Description: enthält Struktur zur Konfiguration und Bits-Position in IO-Stream-Register
  * Created: 12/15/2023 10:00:07 AM
  * Author: Thach
  * Version: 1.0
- * Revision: 1.0
+ * Revision: 1.1
  */ 
 
 
