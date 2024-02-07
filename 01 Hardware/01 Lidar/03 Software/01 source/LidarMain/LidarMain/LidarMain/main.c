7d52268d380da02caa24e1ec83cdbe7987f7966f
hathach23 <thach.hoangha23@gmail.com>
2024-02-07 06:16:42 +0100

    main.c: Beschreibung hinzugefügt

--- 01 Hardware/01 Lidar/03 Software/01 source/LidarMain/LidarMain/LidarMain/main.c
+++ 01 Hardware/01 Lidar/03 Software/01 source/LidarMain/LidarMain/LidarMain/main.c
@@ -1,10 +1,10 @@
 /*
- * LidarMain.c
- *
+ * main.c
+ * Beschreibung: Hauptdatei, wo das Hauptprogramm implementiert wird
  * Created: 12/15/2023 9:08:52 AM
  * Author : Thach
  * Version: 1.1
- * Revision: 1.0
+ * Revision: 1.1
  */ 
 
 #include "main.h"
