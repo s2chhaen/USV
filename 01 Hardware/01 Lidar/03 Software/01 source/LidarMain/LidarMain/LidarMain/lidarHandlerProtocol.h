a574c518676990173719778b3adb0f0d204aee01
hathach23 <thach.hoangha23@gmail.com>
2024-02-07 06:21:15 +0100

    lidarHandlerProtocol.h: Hinzufüge der Beschreibung

--- 01 Hardware/01 Lidar/03 Software/01 source/LidarMain/LidarMain/LidarMain/lidarHandlerProtocol.h
+++ 01 Hardware/01 Lidar/03 Software/01 source/LidarMain/LidarMain/LidarMain/lidarHandlerProtocol.h
@@ -1,10 +1,10 @@
 /*
  * lidarHandlerProtocol.h
- *
+ * Beschreibung: Definition aller Lidar-bezogenen Parameter/Informationen
  * Created: 10/8/2023 3:13:45 PM
  * Author: Thach
  * Version: 1.0
- * Revison: 1.0
+ * Revison: 1.1
  */ 
 
 
