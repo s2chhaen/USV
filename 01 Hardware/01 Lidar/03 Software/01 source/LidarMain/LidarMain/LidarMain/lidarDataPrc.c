9cfce7fff9702c90586b5d3f2b404d7827a23e79
hathach23 <thach.hoangha23@gmail.com>
2024-02-07 06:24:29 +0100

    lidarDataPrc.c: Beschreibung hinzugefügt

--- 01 Hardware/01 Lidar/03 Software/01 source/LidarMain/LidarMain/LidarMain/lidarDataPrc.c
+++ 01 Hardware/01 Lidar/03 Software/01 source/LidarMain/LidarMain/LidarMain/lidarDataPrc.c
@@ -1,10 +1,10 @@
 /*
  * lidarDataPrc.c
- *
+ * Beschreibung: Quellcode-Datei der lidarDataPrc.h
  * Created: 12/1/2023 2:30:54 PM
  * Author: Thach
  * Version: 1.0
- * Revision: 1.0
+ * Revision: 1.1
  */ 
 
 #include "lidarDataPrc.h"
