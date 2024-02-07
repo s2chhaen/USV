63fda818310b649c53cb0c9ca2d83b714bf67a91
hathach23 <thach.hoangha23@gmail.com>
2024-02-07 18:02:21 +0100

    timerUnit.c: Hinzufüge der Beschreibung und Revision

--- 01 Hardware/02 Radar/03 Software/01 source/RadarMain/RadarMain/timerUnit.c
+++ 01 Hardware/02 Radar/03 Software/01 source/RadarMain/RadarMain/timerUnit.c
@@ -1,10 +1,10 @@
 /*
  * timerUnit.c
- *
+ * Description: Quellcode-Datei der timerUnit.h
  * Created: 6/29/2023 11:57:34 PM
  * Author: Thach
  * Version: 1.1
- * Revision: 1.0
+ * Revision: 1.2
  */ 
 
 #include "timerUnit.h"
