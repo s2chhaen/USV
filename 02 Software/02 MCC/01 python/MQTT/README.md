041c1d37ee1dc28be4f5061ce4f50c1964345e2d
Joe G <grabow@amesys.de>
2024-07-15 16:44:50 +0200

    MQTT-Client

---
+++ 02 Software/02 MCC/01 python/MQTT/README.md
@@ -0,0 +1,16 @@
+## Dokumentation zum MQTT-Client des USV 
+
+ Datei | Beschreibung
+-------|-------------
+mqtt_to_sql.json | Konfigurationsdaten für MQTT-Broker und SQL-Server
+mqtt_to_sql.py | MQTT-Client
+mqtt_to_sql.log | Logdatei für MQTT-Client
+
+
+### Übersichtsdarstellung des Datenfluss
+
+![MQTT-Client](mqtt_to_sql.png "MQTT-Client")
+
+
+
+
--- 02 Software/02 MCC/01 python/MQTT/mqtt_to_sql.py
+++ 02 Software/02 MCC/01 python/MQTT/mqtt_to_sql.py
@@ -21,20 +21,33 @@ Program history
 02.02.2024    V. 1.1    SQL-Injektionen verhindern
 05.02.2024    V. 1.2    Fehlerbehandlung
 06.02.2024    V. 1.3    Ausnahme für GPS-Koordinaten
+15.07.2024    V. 1.4    Tabelle angepaßt, Logging hinzugefügt
 
 @author: Prof. Jörg Grabow (grabow@amesys.de)
 """
 
-__version__ = '1.3'
+__version__ = '1.4'
 __author__ = 'Joe Grabow'
 
 
 import json
+import base64
 import paho.mqtt.client as mqtt
 import mysql.connector
 import time
 import atexit
+import logging
 
+
+# logging
+logfile = 'mqtt_to_sql.log'
+
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(levelname)s - %(message)s',
+    filename=logfile,  
+    filemode='w'   
+)
 
 # SQL-Server
 def connect_to_database():
@@ -42,10 +55,10 @@ def connect_to_database():
         conn = mysql.connector.connect(
             host=config["sql"]["host"],
             user=config["sql"]["username"],
-            password=config["sql"]["password"],
+            password=base64.b64decode(config["sql"]["password"]).decode('utf-8'),
             database=config["sql"]["database"]
         )
-        print("Verbindung zur Datenbank erfolgreich hergestellt")
+        logging.info('SQL Datenbank verbunden')
         return conn
     except mysql.connector.Error as e:
         print(f"Fehler bei der Verbindung zur Datenbank: {e}")
@@ -67,25 +80,29 @@ def close_connections():
     client.disconnect()
     if conn:
         conn.close()
-        print("Datenbankverbindung geschlossen")
+        logging.info('SQL Datenbank geschlossen')
+        logging.info('MQTT-Broker getrennt')
 
 
 def on_connect(client, userdata, flags, rc):
     if rc == 0:
-        print("Verbindung zum MQTT-Broker erfolgreich hergestellt")
+        logging.info('MQTT-Broker verbunden')
         # Topic abonnieren (Multi-level-Wildcard) 
         client.subscribe(config["topics"]["main"])
     else:
         print(f"Verbindung fehlgeschlagen, Code: {rc}")
 
 def read_config():
-    with open("mqtt_to_sql_usb.json", "r") as config_file:
+    with open("mqtt_to_sql.json", "r") as config_file:
         config_data = json.load(config_file)
     return config_data
 
 
 # Callback-Funktion für den Empfang von MQTT Nachrichten
 def on_message(client, userdata, msg):
+    global config
+    
+    #print(f"Nachricht empfangen: {msg.payload.decode()} von Topic: {msg.topic}")
     sql_table = config["Table"].get(msg.topic)  # Topic der SQL Tabelle zuordnen
     #sql_table = config["Table"][msg.topic]  # Topic der SQL Tabelle zuordnen
     if not sql_table:
@@ -93,7 +110,7 @@ def on_message(client, userdata, msg):
         return
 
     try:
-        if sql_table == "GPS":  # Ausnahmebehandlung für GPS-Koordinaten
+        if sql_table == "gps":  # Ausnahmebehandlung für GPS-Koordinaten
             gps_position = msg.payload.decode()
             latitude, longitude = map(float, gps_position.split("#"))
             cursor.execute("INSERT INTO {} (Latitude, Longitude) VALUES (%s, %s)".format(sql_table), (latitude, longitude))
@@ -102,16 +119,18 @@ def on_message(client, userdata, msg):
             cursor.execute("INSERT INTO {} (Messung) VALUES (%s)".format(sql_table), (sql_payload,))
 
         conn.commit()
-        print("Erfolgreich eingefügt.")
+        logging.info('SQL Daten geschrieben')
+        #print("Erfolgreich eingefügt.")
     except Exception as e:
         print(f"Fehler beim Einfügen: {e}")
 
     return
 
 
-
 #def main():
 global config
+
+print('MQTT to SQL gestartet')
 
 # Lese die Konfigurationsdaten
 config = read_config()
@@ -126,13 +145,15 @@ client.on_connect = on_connect
 client.on_message = on_message
 
 # Loop starten, um auf Nachrichten zu warten
-client.loop_start()
+# client.loop_forever()
+client.loop_start()  # Starte den Netzwerk-Loop im Hintergrund
     
 # MQTT-Nachrichten empfangen
 try:
     while True:
         time.sleep(0.1)  # kurze Pause, um Nachricht zu empfangen
 except KeyboardInterrupt:
+    client.loop_stop()
     atexit.register(close_connections)
 
 
